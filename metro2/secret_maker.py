c944a9025f33884e72e897908617399f9d1fadea
cooldragontattoo <troy.rttr@gmail.com>
2023-02-03 14:43:12 -0500

    clean up files, add more secret methods

--- metro2/secret_maker.py
+++ metro2/secret_maker.py
@@ -8,12 +8,7 @@ from botocore.exceptions import ClientError
 parser = argparse.ArgumentParser()
 parser.add_argument('--label')
 parser.add_argument('--team')
-parser.add_argument('--host')
-parser.add_argument('--password')
-parser.add_argument('--port')
-parser.add_argument('--engine')
-parser.add_argument('--database')
-parser.add_argument('--user')
+
 
 args = parser.parse_args()
 
@@ -47,20 +42,52 @@ class SecretsManagerSecret:
         else:
             return response
 
-def main(team, label, host, port, database, engine, password, user) -> None: 
+def getClient():
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
         region_name='us-east-1',
     )
+    return client
 
+def writeSecret(team, label, secret_string) -> None: 
+    client = getClient()
     mgr = SecretsManagerSecret(client)
+    mgr.create(f'cfpb/team/{team}/{label}', secret_string) 
 
-    secret_string = f'''"HOST": "{host}","PORT": "{port}","DB": "{database}","ENGINE": "{engine}","PASSWORD": "{password}","USER": "{user}"'''
+def deleteSecret(team, label) -> None:
+    client = getClient()
+    client.delete_secret(
+        SecretId=f'cfpb/team/{team}/{label}',
+        RecoveryWindowInDays=7,
+        ForceDeleteWithoutRecovery=False
+    )
 
-    secret_string_json = "{" + secret_string + "}"
+def get_secret(team, label) -> str:
+    client = getClient()
+    secret_name=f'cfpb/team/{team}/{label}'
 
-    mgr.create(f'cfpb/team/{team}/{label}', secret_string_json) 
-
-
-main(args.team, args.label, args.host, args.port, args.database, args.engine, args.password, args.user)
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            print('The requested secret ' + secret_name + ' was not found')
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            print('The request was invalid due to:', e)
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            print('The request had invalid params:', e)
+        elif e.response['Error']['Code'] == 'DecryptionFailure':
+            print('The requested secret can\'t be decrypted using the provided KMS key:', e)
+        elif e.response['Error']['Code'] == 'InternalServiceError':
+            print('An error occurred on service side:', e)
+        elif e.response['Error']['Code'] == 'ExpiredTokenException': 
+            print('Your AWS session is expired.  Re-run gimme-aws-creds.')
+    else:
+        # Secrets Manager decrypts the secret value using the associated KMS CMK
+        # Depending on whether the secret was a string or binary, only one of these fields will be populated
+        if 'SecretString' in get_secret_value_response:
+            return get_secret_value_response['SecretString']
+        else:
+            binary_secret_data = get_secret_value_response['SecretBinary']
