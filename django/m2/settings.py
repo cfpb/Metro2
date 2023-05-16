85aefe9a6bfbf57175f212a54e60756e3c468491
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2023-05-16 16:31:26 -0400

    Add basic instructions
    Comment out password requirements for now. Fully remove later, if we
    implement LDAP auth.

--- django/m2/settings.py
+++ django/m2/settings.py
@@ -87,18 +87,18 @@ DATABASES = {
 
 # We probably won't need this, since users won't be assigning PWs, right?
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
