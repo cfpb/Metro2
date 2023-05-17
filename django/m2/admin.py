83f3433f9a21e6118d56fe3f9acd604efb9621dc
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2023-05-17 14:50:51 -0400

    Add dataset model with admin management

---
+++ django/m2/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from m2.models import Dataset
+
+admin.site.register(Dataset)
--- django/m2/settings.py
+++ django/m2/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'm2',
 ]
 
 MIDDLEWARE = [
