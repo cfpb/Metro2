d6bd729a6b58be56091f7b2e64ffaf38c089fd3d
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2023-09-22 12:06:05 -0400

    Ingest files from the S3 bucket

--- jobs/parseEvaluate/run.py
+++ jobs/parseEvaluate/run.py
@@ -1,20 +1,16 @@
 import os
 import sys
 import logging
+import tempfile
 
+import s3
 from parse import Parser
 from evaluate import evaluator
 from tables import create_tables, engine
 from envvar import fetch_env_var
 
-# retrieve environment variables. Throw exception if not found.
-try:
-    EXAM_ROOT = os.environ['EXAM_ROOT']
-except KeyError as e:
-    logging.error(f"Postgres connection variable(s) not found: {e}")
-    sys.exit(1)
 
-DATAFILE_PATH = os.path.join(EXAM_ROOT, "data")
+ENABLE_S3 = fetch_env_var('ENABLE_S3', False)
 LOGFILE_LOCATION = fetch_env_var('LOGFILE_LOCATION', "")
 
 
@@ -34,7 +30,55 @@ def parse(fstream, db_connection):
         cursor = db_connection.connection.cursor()
         temp_parser.exec_commands(cursor)
 
-    logging.info(f'File {os.path.basename(fstream.name)} written to database')
+def parse_files_from_s3_bucket(db_connection):
+    exam_root = fetch_env_var('S3_EXAM_ROOT')
+    bucket = s3.getBucket()
+    files = s3.list_objects(bucket, exam_root)
+    logging.info(f"Finding all files in bucket matching prefix '{exam_root}'")
+    for f in files:
+        logging.debug(f"Encountered file in S3: {f.key}")
+        try:
+            # Unforunately, we can't stream files directly from S3 into the parser,
+            # because S3 streambody objects don't have the seek() function, which
+            # is used extensively in the parser. As a workaround, we temporarily
+            # save the file to the local container, then read it from there into
+            # the parser.
+
+            # The file is streamed from S3 in 'bytes' mode
+            temporary_file = tempfile.NamedTemporaryFile(mode='w+b', delete=False)
+            with temporary_file as tf:
+                bucket.Object(f.key).download_fileobj(tf)
+            logging.debug(f"Downloaded file {f.key} successfully.")
+
+            # Open the file in 'text' mode for parsing
+            with open(temporary_file.name, mode='r') as tf:
+                parse(tf, db_connection)
+            logging.info(f'File {f.key} written to database.')
+        finally:
+            os.remove(temporary_file.name)
+
+def parse_files_from_local_filesystem(db_connection):
+    local_exam_root = fetch_env_var('LOCAL_EXAM_ROOT')
+    datafile_path = os.path.join(local_exam_root, "data")
+
+    # iterate over files in [local_exam_root]/data/
+    for filename in os.listdir(datafile_path):
+        logging.debug(f"Encountered file in local data path: {filename}")
+        # checking if the file is a .txt before proceeding
+        if filename.lower().endswith('.txt'):
+            file = os.path.join(datafile_path, filename)
+            # checking if it is a file
+            if os.path.isfile(file):
+                try:
+                    logging.debug(f"Parsing local file: {filename}")
+                    fstream = open(file, 'r')
+                    parse(fstream, db_connection)
+                    logging.info(f'File {os.path.basename(fstream.name)} written to database.')
+                except FileNotFoundError as e:
+                    logging.error(f"There was an error opening the file: {e}")
+                finally:
+                    if fstream:
+                        fstream.close()
 
 def evaluate():
     evaluator.run_evaluators()
@@ -58,23 +102,13 @@ def run():
     init_db(db_engine)
     db_connection = db_engine.connect()
 
-    # iterate over data directory
-    for filename in os.listdir(DATAFILE_PATH):
-        logging.debug(f"Encountered file in local data path: {filename}")
-        # checking if the file is a .txt before proceeding
-        if filename.lower().endswith('.txt'):
-            file = os.path.join(DATAFILE_PATH, filename)
-            # checking if it is a file
-            if os.path.isfile(file):
-                try:
-                    logging.debug(f"Parsing local file: {filename}")
-                    fstream = open(file, 'r')
-                    parse(fstream, db_connection)
-                except FileNotFoundError as e:
-                    logging.error(f"There was an error opening the file: {e}")
-                finally:
-                    if fstream:
-                        fstream.close()
+    if ENABLE_S3:
+        logging.info("ENABLE_S3 set. Reading files from S3 bucket.")
+        parse_files_from_s3_bucket(db_connection)
+    else:
+        logging.info("ENABLE_S3 env var not set. Reading files from local data directory.")
+        parse_files_from_local_filesystem(db_connection)
+
     # TODO: uncomment the evaluate command when we are ready to troubleshoot evaluators
     # evaluate()
 
