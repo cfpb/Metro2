6f8f0d885d063d2230a3e739cd4e1f8b1f6c88d2
Thuy Lam <thuy.lam@cfpb.gov>
2023-09-27 13:33:55 -0400

    Clean up test_logging

--- jobs/parseEvaluate/tests/test_logging.py
+++ jobs/parseEvaluate/tests/test_logging.py
@@ -10,18 +10,17 @@ from logger import getLogger
 
 from tables import meta, header, k1, k2, base, header, trailer, j1, j2, k3, k4, l1, n1
 
-class TestLoggerSetup(TestCase):
-    def test_setup_logger(self):
+class TestLogger(TestCase):
+    def test_get_logger(self):
         logger = getLogger('test')
-        logger.getEffectiveLevel
+
         self.assertTrue(isinstance(logger.handlers[0], logging.StreamHandler))
         self.assertEqual(logger.handlers[0].formatter.datefmt, '%Y-%m-%d %H:%M:%S')
         self.assertEqual(logging.getLevelName(logger.getEffectiveLevel()), logging.getLevelName(logging.DEBUG))
 
 
-    def test_my_function(self):
+    def test_logging(self):
         logger = getLogger('test')
         with self.assertLogs(logger) as log:
             logger.info(111)
-            # or my_function() blablabla... and check only log records:
             self.assertListEqual(log.output, ['INFO:test:111'])
