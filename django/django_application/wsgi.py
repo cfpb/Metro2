9342f63c09d701482e6449c19d5cfacdcea56740
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2023-11-03 14:38:56 -0400

    Bugfix: finish renaming m2 module to django_application
    I forgot to make this change in PR#76.

--- django/django_application/wsgi.py
+++ django/django_application/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'm2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_application.settings')
 
 application = get_wsgi_application()
