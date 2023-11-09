4cf06a787722df7aefe5508a4aa24db90fc68cb4
Thuy Lam <thuy.lam@cfpb.gov>
2023-11-09 16:28:59 -0500

    revert changes on local-py

--- django/django_application/settings/local.py
+++ django/django_application/settings/local.py
@@ -7,11 +7,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'metro2-data',
-        'USER': 'postgres',
-        'PASSWORD': 'cfpb',
-        'HOST': 'postgres',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
