29ea439d98f15c539f51d3149493b45297c6b90a
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2023-11-27 17:39:17 -0500

    Parse entire M2 files

---
+++ django/parse_m2/m2_parser.py
@@ -0,0 +1,244 @@
+import io
+import re
+
+from parse_m2 import fields
+from parse_m2.models import (
+    M2DataFile, UnparseableData,
+    AccountHolder, AccountActivity,
+    J1, J2, K1, K2, K3, K4, L1, N1
+)
+from parse_m2 import parse_utils
+
+
+chunk_size = 2000  # TODO: determine a good number for this
+header_format = r'.{4}HEADER$'
+trailer_format = r'.{4}TRAILER$'
+
+def get_activity_date_from_header(line: str):
+    if re.match(header_format, line[:10]):
+        # If the line is a header, get the activity_date
+        return parse_utils.get_field_value(
+            fields.header_fields["activity_date"],
+            line,
+        )
+    else:
+        raise parse_utils.UnreadableFileException("First line of file isn't a header")
+
+
+def parse_extra_segments(line: str, parsed: dict) -> dict:
+    """
+    Given a string with any number of extra segments, determine what the first
+    segment is, parse its values, add the values to the `parsed` dict, and
+    call this method again with whatever is left of the string.
+    """
+    if len(line) < 2:
+        return parsed
+
+    acct_activity = parsed["AccountActivity"]
+
+    next_segment_indicator = line[:2]
+    if re.match(r'J1', next_segment_indicator):
+        seg_length = fields.seg_length["j1"]
+        j1 = J1.parse_from_segment(line[:seg_length], acct_activity)
+        if "j1" in parsed:
+            parsed["j1"].append(j1)
+        else:
+            parsed["j1"] = [j1]
+
+    elif re.match(r'J2', next_segment_indicator):
+        seg_length = fields.seg_length["j2"]
+        j2 = J2.parse_from_segment(line[:seg_length], acct_activity)
+        if "j2" in parsed:
+            parsed["j2"].append(j2)
+        else:
+            parsed["j2"] = [j2]
+
+    elif re.match(r'K1', next_segment_indicator):
+        seg_length = fields.seg_length["k1"]
+        k1 = K1.parse_from_segment(line[:seg_length], acct_activity)
+        parsed["k1"] = k1
+
+    elif re.match(r'K2', next_segment_indicator):
+        seg_length = fields.seg_length["k2"]
+        k2 = K2.parse_from_segment(line[:seg_length], acct_activity)
+        parsed["k2"] = k2
+
+    elif re.match(r'K3', next_segment_indicator):
+        seg_length = fields.seg_length["k3"]
+        k3 = K3.parse_from_segment(line[:seg_length], acct_activity)
+        parsed["k3"] = k3
+
+    elif re.match(r'K4', next_segment_indicator):
+        seg_length = fields.seg_length["k4"]
+        k4 = K4.parse_from_segment(line[:seg_length], acct_activity)
+        parsed["k4"] = k4
+
+    elif re.match(r'L1', next_segment_indicator):
+        seg_length = fields.seg_length["l1"]
+        l1 = L1.parse_from_segment(line[:seg_length], acct_activity)
+        parsed["l1"] = l1
+
+    elif re.match(r'N1', next_segment_indicator):
+        seg_length = fields.seg_length["n1"]
+        n1 = N1.parse_from_segment(line[:seg_length], acct_activity)
+        parsed["n1"] = n1
+
+    else:
+        msg = f"Extra segment indicator `{next_segment_indicator}`" + \
+            " did not match any valid extra segment"
+        raise parse_utils.UnreadableLineException(msg)
+
+    if seg_length:
+        parse_extra_segments(line[seg_length:], parsed)
+
+    return parsed
+
+def parse_line(line: str, file_record: M2DataFile, activity_date) -> dict:
+    """
+    Given a single line of a Metro2 file, parse all of the segments it contains
+    into individual model records, and put them all into a dict.
+
+    inputs:
+    - line: the line of metro2 data, starting with a base segment
+    - file_record: the M2DataFile that represents the file that the
+                   line of data came from. Used as a foreign key when
+                   saving the records in the line.
+    - activity_date: date object, to be saved in the AccountHolder and
+                     AccountActivity records for this line
+
+    output:
+    - If the line was a valid Metro2 line, a dict with the following keys:
+      AccountHolder, AccountActivity, and optionally a key for each
+      type of extra segment that was present in the line
+      (j1, j2, k1, k2, k3, k4, l1, n1)
+    - If the line was not valid (i.e. if any part of the parsing process
+      threw an UnreadableLine exception), a dict with an 'UnparseableData' key.
+    """
+    if re.match(trailer_format, line[:11]):
+        # If the line is a trailer, ignore it
+        return
+
+    try:
+        parsed = {}
+        # parse the base segment into AccountHolder and AccountActivity
+        acct_holder = AccountHolder.parse_from_segment(
+            line, file_record, activity_date)
+        parsed["AccountHolder"] = acct_holder
+
+        acct_activity = AccountActivity.parse_from_segment(
+            line, acct_holder, activity_date)
+        parsed["AccountActivity"] = acct_activity
+
+        # parse the extra segments
+        base_segment_length = fields.seg_length["header"]
+        remaining_chars = line[base_segment_length:]
+        parsed = parse_extra_segments(remaining_chars, parsed)
+        return parsed
+
+    except parse_utils.UnreadableLineException as e:
+        # if any part of the line couldn't be parsed, don't save
+        #  the segments; only save the line as UnparseableData
+        return {"UnparseableData": UnparseableData(
+            data_file=file_record,
+            unparseable_line=line,
+            error_description=str(e)
+        )}
+
+def parse_chunk(f: io.TextIOWrapper, chunk_size: int, file_record: M2DataFile, activity_date) -> dict:
+    """
+    Given a filestream of a Metro2 file, parse chunk_size lines from it and save them
+    to a dict.
+
+    inputs:
+    - f: filestream of the Metro2 file
+    - chunk_size: the number of lines to parse before returning
+    - file_record: the M2DataFile that represents the file that the
+                   line of data came from. Used as a foreign key when
+                   saving the records in each line.
+    - activity_date: date object, to be saved in the AccountHolder and
+                     AccountActivity records for each line
+
+    output:
+    - a dict with the following keys: AccountHolder, AccountActivity,
+      extra segment keys (if any were present), and UnparseableData (if any)
+    """
+    parsed_records = {
+        "AccountHolder": [],
+        "AccountActivity": [],
+        "j1": [],
+        "j2": [],
+        "k1": [],
+        "k2": [],
+        "k3": [],
+        "k4": [],
+        "l1": [],
+        "n1": [],
+        "UnparseableData": [],
+    }
+    lines_parsed = 0
+    while lines_parsed < chunk_size:
+        line = f.readline()
+        if not line:
+            # If the file has ended, exit the parser
+            break
+
+        line_results = parse_line(line, file_record, activity_date)
+        if line_results:
+            if "UnparseableData" in line_results:
+                parsed_records["UnparseableData"].append(line_results["UnparseableData"])
+            if "AccountHolder" in line_results:
+                parsed_records["AccountHolder"].append(line_results["AccountHolder"])
+            if "AccountActivity" in line_results:
+                parsed_records["AccountActivity"].append(line_results["AccountActivity"])
+            if "j1" in line_results:
+                parsed_records["j1"] = parsed_records["j1"] + line_results["j1"]
+            if "j2" in line_results:
+                parsed_records["j2"] = parsed_records["j2"] + line_results["j2"]
+            if "k1" in line_results:
+                parsed_records["k1"].append(line_results["k1"])
+            if "k2" in line_results:
+                parsed_records["k2"].append(line_results["k2"])
+            if "k3" in line_results:
+                parsed_records["k3"].append(line_results["k3"])
+            if "k4" in line_results:
+                parsed_records["k4"].append(line_results["k4"])
+            if "l1" in line_results:
+                parsed_records["l1"].append(line_results["l1"])
+            if "n1" in line_results:
+                parsed_records["n1"].append(line_results["n1"])
+
+        lines_parsed += 1
+
+    return parsed_records
+
+def save_values_bulk(values: dict):
+    UnparseableData.objects.bulk_create(values["UnparseableData"])
+    AccountHolder.objects.bulk_create(values["AccountHolder"])
+    AccountActivity.objects.bulk_create(values["AccountActivity"])
+    J1.objects.bulk_create(values["j1"])
+    J2.objects.bulk_create(values["j2"])
+    K1.objects.bulk_create(values["k1"])
+    K2.objects.bulk_create(values["k2"])
+    K3.objects.bulk_create(values["k3"])
+    K4.objects.bulk_create(values["k4"])
+    L1.objects.bulk_create(values["l1"])
+    N1.objects.bulk_create(values["n1"])
+
+def parse_file_contents(file_record: M2DataFile, f: io.TextIOWrapper, file_size: int):
+    """
+    Parse a Metro2 file and save the records to the database.
+
+    Inputs:
+    `file_record` - instance of M2DataFile, which will act as the parent record for all
+                    data records parsed from this file
+    `f` - file stream of the data file to be parsed
+    `file_size` - the size of the file stream in bytes
+    """
+    # get first line
+    header_line = f.readline()
+    activity_date = get_activity_date_from_header(header_line)
+
+    # parse the rest of the file until it is done
+    while f.tell() < file_size:
+        values = parse_chunk(f, chunk_size, file_record, activity_date)
+        save_values_bulk(values)
---
+++ django/parse_m2/tests/test_parser.py
@@ -0,0 +1,141 @@
+from django.test import TestCase
+import os
+from datetime import datetime
+
+from parse_m2 import m2_parser
+from parse_m2 import parse_utils
+from parse_m2.models import (
+    M2DataFile, UnparseableData,
+    AccountHolder, AccountActivity,
+    J1, J2, K1, K2, K3, K4, L1, N1
+)
+
+class ParserTestCase(TestCase):
+    def setUp(self):
+        self.header_seg = os.path.join('parse_m2', 'tests','sample_files', 'header_segment_1.txt')
+        self.base_seg = os.path.join('parse_m2', 'tests','sample_files', 'base_segment_1.txt')
+        self.tiny_file = os.path.join('parse_m2', 'tests','sample_files', 'm2_file_small.txt')
+        self.error_file = os.path.join('parse_m2', 'tests','sample_files', 'm2_file_small_with_error.txt')
+
+        self.extras_str = "K1ORIGNALCREDITORNAME           03K22SOLDTONAME                     L12NEWACCTNUMBER                                      "
+
+        self.data_file = M2DataFile(exam_identifier="test_exam", file_name="file.txt")
+        self.data_file.save()
+        self.activity_date = datetime(2021, 1, 1)
+        self.account_holder = AccountHolder(
+            data_file = self.data_file, activity_date = self.activity_date)
+        self.account_activity = AccountActivity(
+            account_holder = self.account_holder, activity_date = self.activity_date)
+
+    ############################
+    # Tests for the whole parsing process
+    def test_parse(self):
+        # Test the whole parsing process for a file
+        file_size = os.path.getsize(self.tiny_file)
+
+        with open(self.tiny_file, mode='r') as filestream:
+            m2_parser.parse_file_contents(self.data_file, filestream, file_size)
+
+            # The test file contains the following segments:
+            self.assertEqual(AccountHolder.objects.count(), 3)
+            self.assertEqual(AccountActivity.objects.count(), 3)
+            self.assertEqual(K1.objects.count(), 1)
+            self.assertEqual(K2.objects.count(), 1)
+            self.assertEqual(L1.objects.count(), 1)
+
+            # The test file does not contain these:
+            self.assertEqual(UnparseableData.objects.count(), 0)
+            self.assertEqual(J1.objects.count(), 0)
+            self.assertEqual(J2.objects.count(), 0)
+            self.assertEqual(K3.objects.count(), 0)
+            self.assertEqual(K4.objects.count(), 0)
+            self.assertEqual(N1.objects.count(), 0)
+
+    def test_parse_file_with_error(self):
+        # error_file is identical to _tiny_file except that one line was modified
+        # to make it unparseable
+        file_size = os.path.getsize(self.error_file)
+        with open(self.error_file, mode='r') as filestream:
+            m2_parser.parse_file_contents(self.data_file, filestream, file_size)
+
+            # The test file contains the following segments:
+            self.assertEqual(UnparseableData.objects.count(), 1)
+            self.assertEqual(AccountHolder.objects.count(), 2)
+            self.assertEqual(AccountActivity.objects.count(), 2)
+            self.assertEqual(K1.objects.count(), 1)
+            self.assertEqual(K2.objects.count(), 1)
+
+
+    ############################
+    # Tests for handling unparseable data
+    def test_unparseable_data_in_line(self):
+        line = "this is a bad line of data"
+        result = m2_parser.parse_line(line, self.data_file, self.activity_date)
+        # result contains an instance of UnparseableData
+        unparseable = result["UnparseableData"]
+        self.assertEqual(unparseable.unparseable_line, line)
+        self.assertIn("Segment too short", unparseable.error_description)
+
+        # since the line was unparseable, no segments were added to result
+        self.assertNotIn("j1", result)
+        self.assertNotIn("j2", result)
+        self.assertNotIn("k1", result)
+        self.assertNotIn("k2", result)
+        self.assertNotIn("k3", result)
+        self.assertNotIn("k4", result)
+        self.assertNotIn("l1", result)
+        self.assertNotIn("n1", result)
+
+    ############################
+    # Tests for parsing extra segments
+    def test_parsing_extra_segments(self):
+        records = {"AccountActivity": self.account_activity}
+        result = m2_parser.parse_extra_segments(self.extras_str, records)
+        self.assertNotIn("j1", result)
+        self.assertNotIn("j2", result)
+        self.assertEqual(result["k1"].orig_creditor_name, "ORIGNALCREDITORNAME")
+        self.assertEqual(result["k2"].purch_sold_name, "SOLDTONAME")
+        self.assertEqual(result["l1"].new_acc_num, "NEWACCTNUMBER")
+        self.assertNotIn("k4", result)
+        self.assertNotIn("n1", result)
+
+    def test_no_extra_segments_exist(self):
+        records = {"AccountActivity": self.account_activity}
+        result = m2_parser.parse_extra_segments("", records)
+        self.assertEqual(result, records)
+
+    def test_extra_segment_too_short(self):
+        records = {"AccountActivity": self.account_activity}
+        str = "K2 but is too short for a k2"
+        with self.assertRaises(parse_utils.UnreadableLineException):
+            m2_parser.parse_extra_segments(str, records)
+
+    def test_extra_segment_doesnt_match_any_type(self):
+        records = {"AccountActivity": self.account_activity}
+        str = "W2 is not a type of extra segment"
+        with self.assertRaises(parse_utils.UnreadableLineException):
+            m2_parser.parse_extra_segments(str, records)
+
+    ############################
+    # Tests for parsing the header
+    def test_header_doesnt_match_format(self):
+        bad_str = "this string is not a header segment"
+        with self.assertRaises(parse_utils.UnreadableFileException):
+            m2_parser.get_activity_date_from_header(bad_str)
+
+    def test_header_with_malformed_activity_date(self):
+        # 99-99-2023 is not a valid date
+        bad_str = "xxxxHEADERxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx99992023xxxxxxxxxxxx"
+        with self.assertRaises(parse_utils.UnreadableLineException):
+            m2_parser.get_activity_date_from_header(bad_str)
+
+    def test_header_too_short(self):
+        bad_str = "xxxxHEADER with insufficient characters"
+        with self.assertRaises(parse_utils.UnreadableLineException):
+            m2_parser.get_activity_date_from_header(bad_str)
+
+    def test_get_activity_date_from_header(self):
+        with open(self.header_seg, mode='r') as file:
+            header_row = file.readline()
+            activity_date = m2_parser.get_activity_date_from_header(header_row)
+            self.assertEqual(activity_date, datetime(2023, 12, 31, 0, 0))
