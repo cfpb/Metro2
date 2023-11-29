4c1d047b3e586d456d2897397817e6a11492bbb2
Thuy Lam <thuy.lam@cfpb.gov>
2023-11-29 15:29:39 -0500

    simplify test bulk activity

--- django/evaluate_m2/tests/evaluator_test_helper.py
+++ django/evaluate_m2/tests/evaluator_test_helper.py
@@ -16,40 +16,43 @@ class EvaluatorTestHelper():
             )
         return AccountHolder.objects.bulk_create(account_holders)
 
-    def create_bulk_activities(self, value_list: dict, size: int):
+    def create_bulk_activities(self, data_file: M2DataFile, value_list: dict, size: int):
         account_activities=[]
 
         for i in range(0, size):
             account_activities.append(self.create_acct_activity(
-                id=self.get_field_value(value_list['id']
-                    if "id" in value_list else (), i, 1),
-                account_holder=self.get_account_holder(value_list['account_holder'], i),
-                acct_stat=self.get_field_value( value_list['acct_stat']
-                    if "acct_stat" in value_list else (), i, '00'),
-                acct_type=self.get_field_value(value_list['acct_type']
-                    if "acct_type" in value_list else (), i, '00'),
-                amt_past_due=self.get_field_value(value_list['amt_past_due']
-                    if "amt_past_due" in value_list else (), i, 0),
-                cons_acct_num=self.get_field_value(value_list['cons_acct_num']
-                    if "cons_acct_num" in value_list else (), i, '012345'),
-                credit_limit=self.get_field_value(value_list['credit_limit']
-                    if "credit_limit" in value_list else (), i, 0),
-                current_bal=self.get_field_value(value_list['current_bal']
-                    if "current_bal" in value_list else (), i, 0),
-                dofd=self.get_field_value(value_list['dofd']
-                    if "dofd" in value_list else (), i, datetime(2020, 1, 1)),
-                hcola=self.get_field_value(value_list['hcola']
-                    if "hcola" in value_list else (), i, 0),
-                port_type=self.get_field_value(value_list['port_type']
-                    if "port_type" in value_list else (), i, 'X'),
-                pmt_rating=self.get_field_value(value_list['pmt_rating']
-                    if "pmt_rating" in value_list else (), i, '0'),
-                spc_com_cd=self.get_field_value(value_list['spc_com_cd']
-                    if "spc_com_cd" in value_list else (), i, 'X'),
-                terms_dur=self.get_field_value(value_list['terms_dur']
-                    if "terms_dur" in value_list else (), i, '0'),
-                terms_freq=self.get_field_value(value_list['terms_freq']
-                    if "terms_freq" in value_list else (), i, '0')
+                id=value_list['id'][i]
+                    if "id" in value_list else 1,
+                account_holder=AccountHolder.objects.get(
+                    cons_info_ind=value_list['account_holder'][i])
+                    if "account_holder" in value_list
+                    else self.create_acct_holder(data_file),
+                acct_stat=value_list['acct_stat'][i]
+                    if "acct_stat" in value_list else '00',
+                acct_type=value_list['acct_type'][i]
+                    if "acct_type" in value_list else '00',
+                amt_past_due=value_list['amt_past_due'][i]
+                    if "amt_past_due" in value_list else 0,
+                cons_acct_num=value_list['cons_acct_num'][i]
+                    if "cons_acct_num" in value_list else '012345',
+                credit_limit=value_list['credit_limit'][i]
+                    if "credit_limit" in value_list else 0,
+                current_bal=value_list['current_bal'][i]
+                    if "current_bal" in value_list else 0,
+                dofd=value_list['dofd'][i]
+                    if "dofd" in value_list else datetime(2020, 1, 1),
+                hcola=value_list['hcola'][i]
+                    if "hcola" in value_list else 0,
+                port_type=value_list['port_type'][i]
+                    if "port_type" in value_list else 'X',
+                pmt_rating=value_list['pmt_rating'][i]
+                    if "pmt_rating" in value_list else '0',
+                spc_com_cd=value_list['spc_com_cd'][i]
+                    if "spc_com_cd" in value_list else 'X',
+                terms_dur=value_list['terms_dur'][i]
+                    if "terms_dur" in value_list else '0',
+                terms_freq=value_list['terms_freq'][i]
+                    if "terms_freq" in value_list else '0'
                 ))
         return AccountActivity.objects.bulk_create(account_activities)
 
@@ -60,11 +63,14 @@ class EvaluatorTestHelper():
             cons_info_ind=cons_info_ind)
 
     def create_acct_activity(self, id: int, account_holder: AccountHolder,
-        acct_stat: str, acct_type: str, amt_past_due: int, cons_acct_num: int, credit_limit: int, current_bal: int, hcola: int, port_type: str,
+        acct_stat: str, acct_type: str, amt_past_due: int, cons_acct_num: int,
+        credit_limit: int, current_bal: int, hcola: int, port_type: str,
         spc_com_cd: str, terms_dur: str, terms_freq: str, dofd: datetime,
         pmt_rating: str, actual_pmt_amt=0, orig_chg_off_amt=0, smpa=0):
         return AccountActivity(
-            id=id, account_holder=account_holder, acct_stat=acct_stat, acct_type=acct_type, activity_date=self.activity_date, amt_past_due=amt_past_due, actual_pmt_amt=actual_pmt_amt, cons_acct_num=cons_acct_num, credit_limit=credit_limit, current_bal=current_bal, date_closed=self.date, date_open=self.date, doai=self.date, hcola=hcola, orig_chg_off_amt=orig_chg_off_amt, port_type=port_type, smpa=smpa, spc_com_cd=spc_com_cd, terms_dur=terms_dur, terms_freq=terms_freq, dofd=dofd, pmt_rating=pmt_rating)
+            id=id, account_holder=account_holder, acct_stat=acct_stat, acct_type=acct_type,
+            activity_date=self.activity_date, amt_past_due=amt_past_due, actual_pmt_amt=actual_pmt_amt,
+            cons_acct_num=cons_acct_num, credit_limit=credit_limit, current_bal=current_bal, date_closed=self.date, date_open=self.date, doai=self.date, hcola=hcola, orig_chg_off_amt=orig_chg_off_amt, port_type=port_type, smpa=smpa, spc_com_cd=spc_com_cd, terms_dur=terms_dur, terms_freq=terms_freq, dofd=dofd, pmt_rating=pmt_rating)
 
     def create_jsegment(self, id: int, j_type: str, cons_info_ind: str):
         if j_type == 'j1':
@@ -75,20 +81,11 @@ class EvaluatorTestHelper():
     def create_k2(self, id: int, purch_sold_ind: str, purch_sold_name: str):
         return  K2(account_activity=AccountActivity.objects.get(id=id), purch_sold_ind=purch_sold_ind, purch_sold_name=purch_sold_name)
 
-    def get_account_holder(self, data: str, index: int):
-        if (len(data) > 1):
-            return AccountHolder.objects.get(cons_info_ind=data[index])
-        else:
-            return AccountHolder.objects.get(cons_info_ind=data[0])
 
-    def get_field_value(self, data: tuple, index: str, default):
-        if data:
-            return data[index]
-        else:
-            return default
-
-    def create_l1(self, id=1, change_ind='1', new_acc_num='9876543210', new_id_num='1234567890'):
-        return  L1(account_activity=AccountActivity.objects.get(id=id), change_ind=change_ind, new_acc_num=new_acc_num, new_id_num=new_id_num)
+    def create_l1(self, id=1, change_ind='1', new_acc_num='9876543210',
+                  new_id_num='1234567890'):
+        return  L1(account_activity=AccountActivity.objects.get(id=id),
+                   change_ind=change_ind, new_acc_num=new_acc_num, new_id_num=new_id_num)
 
     def assert_evaluator_correct(self, eval_name: str, expected_result: list[dict]):
         # Test that the evaluator:
