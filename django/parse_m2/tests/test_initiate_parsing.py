53d09c18b97217aafb40b2cde1d2fa987a1496e0
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2023-12-07 17:17:46 -0500

    Improve tests

--- django/parse_m2/tests/test_initiate_parsing.py
+++ django/parse_m2/tests/test_initiate_parsing.py
@@ -1,9 +1,8 @@
 import os
 from django.test import TestCase
-from unittest.mock import patch
 
-from parse_m2.initiate_parsing import parse_files_from_local_filesystem
-from parse_m2.models import M2DataFile, AccountHolder
+from parse_m2.initiate_parsing import parse_files_from_local_filesystem, parse_files_from_s3_bucket
+from parse_m2.models import Metro2Event, M2DataFile, AccountHolder
 
 
 class InitiateParsingTestCase(TestCase):
@@ -15,8 +14,13 @@ class InitiateParsingTestCase(TestCase):
             )
 
     def test_open_local_files(self):
-        exam_id = "examination A"
-        parse_files_from_local_filesystem(exam_id, self.test_local_data_directory)
+        parse_files_from_local_filesystem("exam Z", self.test_local_data_directory)
+
+        # Should create one event
+        self.assertEqual(Metro2Event.objects.count(), 1)
+        event = Metro2Event.objects.first()
+        self.assertEqual(event.name, "exam Z")
+
         # one M2DataFile object for each file that was opened
         self.assertEqual(M2DataFile.objects.count(), 2)
         # 3 records in the first file, 2 in the second
@@ -27,3 +31,21 @@ class InitiateParsingTestCase(TestCase):
         # this exception is uncaught.
         with self.assertRaises(FileNotFoundError):
             parse_files_from_local_filesystem("exam A", "/directory/that/does/not/exist")
+
+    # Test for parsing files from the S3 bucket. Only run when testing manually.
+    # Before running, make sure S3 env vars are in place.
+    def xtest_fetch_s3(self):
+        parse_files_from_s3_bucket("exam B", "test-tiny")
+
+        # Should create one event
+        self.assertEqual(Metro2Event.objects.count(), 1)
+        event = Metro2Event.objects.first()
+        self.assertEqual(event.name, "exam B")
+
+        # The test directory in S3 should contain one file
+        self.assertEqual(M2DataFile.objects.count(), 1)
+        file = M2DataFile.objects.first()
+        self.assertEqual(file.file_name, "s3:test-tiny/m2_2k_lines_deidentified.TXT")
+
+        # The test file should contain 1998 base segments
+        self.assertEqual(AccountHolder.objects.count(), 1998)
