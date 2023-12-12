fa64f458e953b1134c4c5394f6d6d440ad17c5c4
Thuy Lam <thuy.lam@cfpb.gov>
2023-12-12 10:57:17 -0500

    update code for new schema

--- django/evaluate_m2/models.py
+++ django/evaluate_m2/models.py
@@ -1,15 +1,43 @@
+
+import logging
+
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.db.models import JSONField
 
 from parse_m2.models import AccountActivity
 
+
 class EvaluatorMetaData(models.Model):
-    evaluator_name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200, blank=False)
+    func: any
+    longitudinal_func: any
+
+    def set_evaluator_properties(self, func = None, longitudinal_func = None):
+        self.func = func
+        self.longitudinal_func = longitudinal_func
+
+
+    # def all_account_activity(self):
+    #     return self.objects.evaluator.source_record__set.all()
+    def exec_custom_func(self) -> list[dict]:
+        # returns a list of results from running a query
+        logger = logging.getLogger('evaluator.exec_custom_func')
+        res = list()
+        if self.longitudinal_func:
+            res = self.longitudinal_func
+        else:
+            res = list(self.func)
+
+        return res
+
+class EvaluatorResultSummary(models.Model):
+    evaluator_name = models.ForeignKey(EvaluatorMetaData, on_delete=models.CASCADE)
     hits = models.IntegerField()
 
 class EvaluatorResult(models.Model):
-    evaluator = models.ForeignKey(EvaluatorMetaData, on_delete=models.CASCADE)
+    result_summary = models.ForeignKey(EvaluatorResultSummary, on_delete=models.CASCADE)
     date = models.DateField()
-    field_values = JSONField()
+    field_values = JSONField(encoder=DjangoJSONEncoder)
     source_record = models.ForeignKey(AccountActivity, on_delete=models.CASCADE)
     acct_num = models.CharField(max_length=30)
