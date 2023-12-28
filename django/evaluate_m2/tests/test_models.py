2f54901b26565303143e3884ef746f77bc4c8415
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2023-12-28 14:23:45 -0500

    Small test refactor

--- django/evaluate_m2/tests/test_models.py
+++ django/evaluate_m2/tests/test_models.py
@@ -5,11 +5,10 @@ from evaluate_m2.tests.evaluator_test_helper import EvaluatorTestHelper
 
 
 class EvaluateModelsTestCase(TestCase, EvaluatorTestHelper):
+    mock_function = lambda: "mock output"
+
     def test_set_func_sets_func(self):
         evl = EvaluatorMetadata(name="Test")
-        evl.set_func(func=self.set_mock_function)
+        evl.set_func(func=self.mock_function)
 
-        self.assertEqual(self.set_mock_function, evl.func)
-
-    def set_mock_function(self):
-        return 'test'
+        self.assertEqual(self.mock_function, evl.func)
