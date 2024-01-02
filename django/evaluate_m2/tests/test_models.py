7b760b30119a6de7e8b9ab119c1bb2db994f3db1
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2024-01-02 15:12:53 -0500

    set_func was removed; fix test

--- django/evaluate_m2/tests/test_models.py
+++ django/evaluate_m2/tests/test_models.py
@@ -5,14 +5,6 @@ from evaluate_m2.tests.evaluator_test_helper import EvaluatorTestHelper
 
 
 class EvaluateModelsTestCase(TestCase, EvaluatorTestHelper):
-    mock_function = lambda: "mock output"
-
-    def test_set_func_sets_func(self):
-        evl = EvaluatorMetadata(name="Test")
-        evl.set_func(func=self.mock_function)
-
-        self.assertEqual(self.mock_function, evl.func)
-
     def test_eval_create_from_dict(self):
         input_json = {
             'name': 'ADDL-DOFD-1',
