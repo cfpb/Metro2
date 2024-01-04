a017c0ccd982ef8ada5f576e75585749de9abf34
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2024-01-04 16:40:21 -0500

    Improve test coverage by testing evaluator export endpoint

--- django/evaluate_m2/views.py
+++ django/evaluate_m2/views.py
@@ -8,7 +8,7 @@ def download_evaluator_metadata(request):
     # Documentation on returning CSV: https://docs.djangoproject.com/en/4.2/howto/outputting-csv/
     filename = f"evaluator-metadata-{date.today()}.csv"
     response = HttpResponse(
-        content_type="test/csv",
+        content_type="text/csv",
         headers={"Content-Disposition": f"attachment; filename={filename}"}
     )
 
