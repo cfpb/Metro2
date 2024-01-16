6dac9dec0b3b7b6b518a024233df8a3ed94552f1
Thuy Lam <thuy.lam@cfpb.gov>
2024-01-16 11:50:51 -0500

    add endpoint and tests

--- django/evaluate_m2/tests/test_models.py
+++ django/evaluate_m2/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
-from evaluate_m2.models import EvaluatorMetadata
+from evaluate_m2.models import EvaluatorMetadata, EvaluatorResult, EvaluatorResultSummary
 from evaluate_m2.tests.evaluator_test_helper import EvaluatorTestHelper
+from parse_m2.models import Metro2Event
 
 
 class EvaluateModelsTestCase(TestCase, EvaluatorTestHelper):
@@ -78,3 +79,37 @@ class EvaluateModelsTestCase(TestCase, EvaluatorTestHelper):
             "Low",                      # self.risk_level
         ]
         self.assertEqual(eval1.serialize(), expected)
+
+    def test_eval_res_create_csv_header(self):
+        field_values_json = {
+            'field1': 'value1',
+            'field2': 'value2',
+            'field3': 'value3',
+        }
+        eval_res = EvaluatorResult(
+            result_summary = EvaluatorResultSummary(),
+            field_values = field_values_json,
+        )
+        expected = [ 'name', 'field1', 'field2', 'field3' ]
+
+        self.assertEqual(eval_res.create_csv_header(), expected)
+
+    def test_eval_res_create_csv_row_data(self):
+        eval_rs1 = EvaluatorResultSummary(
+            event = Metro2Event(name = 'test'),
+            evaluator = EvaluatorMetadata(),
+            hits = 1
+        )
+        field_values_json = {
+            'field1': 'value1',
+            'field2': 'value2',
+            'field3': 'value3',
+        }
+
+        eval_res = EvaluatorResult(
+            result_summary = eval_rs1,
+            field_values = field_values_json,
+        )
+        expected = [ 'test', 'value1', 'value2', 'value3' ]
+
+        self.assertEqual(eval_res.create_csv_row_data(), expected)
