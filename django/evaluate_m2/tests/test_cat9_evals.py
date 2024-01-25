77b795dbbcd43495087dd47373fb2aac463bacff
Thuy Lam <thuy.lam@cfpb.gov>
2024-01-25 16:57:37 -0500

    add additional tests for 9-3A

--- django/evaluate_m2/tests/test_cat9_evals.py
+++ django/evaluate_m2/tests/test_cat9_evals.py
@@ -20,19 +20,9 @@ class Cat9_EvalsTestCase(TestCase, EvaluatorTestHelper):
         self.data_file.save()
         # Create the Account Holders
         self.accounts = self.create_bulk_account_holders(self.data_file, ('A','B','C','D','E','F','G','H'))
+        self.prev_j1 = []
 
-    ############################
-    # Tests for the category 9 evaluators
-    def test_eval_9_3A_acct_stat_current_PHP_not_current(self):
-    # Hits when conditions met:
-    # 1. prior_cons_info_ind=''
-    # 2. any (prior_j1.cons_info_ind == '')
-    # 3. any (prior_j2.cons_info_ind == '')
-    # 4. prior_acct_stat == '11'
-    # 5. prior_current_bal > 0
-    # 6. port_type  == 'C', 'O', 'R'
-    # 7. php != 'O'
-
+    def create_test_9_3a_records(self, create_all_j1_segments: bool):
         # Create previous Account Activities data
         prev_activities = { 'id':(32,33,34,35,36,37,38,39),
             'activity_date':(date(2019, 11, 30), date(2019, 11, 30), date(2019, 11, 30),
@@ -44,14 +34,21 @@ class Cat9_EvalsTestCase(TestCase, EvaluatorTestHelper):
             'current_bal':(5, 10, 15, 20, 25, 0, 35, 40)}
         self.create_bulk_activities(self.prev_data_file, prev_activities, 8)
 
-        prev_j1_data = {
-              'account_activity':(32,33,34,35,36,37,38,39),
-              'cons_info_ind':('','','','','M','','','')}
-        self.create_bulk_JSegments('j1', prev_j1_data, 8)
+        if create_all_j1_segments:
+            prev_j1_data = {
+                'account_activity':(32,33,34,35,36,37,38,39),
+                'cons_info_ind':('','','','','M','','','')}
+            self.prev_j1 = self.create_bulk_JSegments('j1', prev_j1_data, 8)
+        else:
+            prev_j1_data = {
+                'account_activity':(32,34,35,36,37,38,39),
+                'cons_info_ind':('','','','M','','','')}
+            self.prev_j1 = self.create_bulk_JSegments('j1', prev_j1_data, 7)
+
 
         prev_j2_data = {
-              'account_activity':(32,33,34,35,36,37,38,39),
-              'cons_info_ind':('','','','','','','','')}
+            'account_activity':(32,33,34,35,36,37,38,39),
+            'cons_info_ind':('','','','','','','','')}
         self.create_bulk_JSegments('j2', prev_j2_data, 8)
 
         # Create the Account Activities data
@@ -62,15 +59,21 @@ class Cat9_EvalsTestCase(TestCase, EvaluatorTestHelper):
             'php':('LLL','LLL','LLL','LLL','LLL','LLL','LLL','OLL')}
         self.create_bulk_activities(self.data_file, activities, 8)
 
-        j1_data = {
-              'account_activity':(42,43,44,45,46,47,48,49),
-              'cons_info_ind':('A','B','C','D','E','F','G','H')}
-        self.create_bulk_JSegments('j1', j1_data, 8)
+    ############################
+    # Tests for the category 9 evaluators
 
-        j2_data = {
-              'account_activity':(42,43,44,45,46,47,48,49),
-              'cons_info_ind':('A','B','C','D','E','F','G','H')}
-        self.create_bulk_JSegments('j2', j2_data, 8)
+    # Hits when conditions met:
+    # 1. prior_cons_info_ind=''
+    # 2. any (prior_j1.cons_info_ind == '')
+    # 3. any (prior_j2.cons_info_ind == '')
+    # 4. prior_acct_stat == '11'
+    # 5. prior_current_bal > 0
+    # 6. port_type  == 'C', 'O', 'R'
+    # 7. php != 'O'
+
+    def test_eval_9_3A_acct_stat_current_PHP_not_current(self):
+        # Create previous Account Activities data
+        self.create_test_9_3a_records(create_all_j1_segments=True)
         # 1: HIT, 2: HIT, 3: NO-port_type='A', 4: NO-prev_cons_info_ind='A',
         # 5: NO-prev_j1__cons_info_ind='M', 6: NO-prev_current_bal=0,
         # 7: NO-prev_acct_stat=10, 8: NO-php=O
@@ -88,5 +91,60 @@ class Cat9_EvalsTestCase(TestCase, EvaluatorTestHelper):
             'prev_acct_stat':'11', 'prev_cons_info_ind': '',
             'prev_j1__cons_info_ind': '', 'prev_j2__cons_info_ind': '',
             'prev_current_bal': 10
+        }]
+        self.assert_evaluator_correct(self.event, '9-3A Account Status current but PHP not current', expected)
+
+
+    def test_eval_9_3A_acct_stat_current_PHP_missing_j1_segment(self):
+        # Create previous Account Activities data
+        self.create_test_9_3a_records(create_all_j1_segments=False)
+        # 1: HIT, 2: No-missing J1 segment, 3: NO-port_type='A',
+        # 4: NO-prev_cons_info_ind='A', 5: NO-prev_j1__cons_info_ind='M',
+        # 6: NO-prev_current_bal=0, 7: NO-prev_acct_stat=10, 8: NO-php=O
+
+        # Create the segment data
+        expected = [{
+            'id': 42, 'activity_date': date(2019, 12, 31),
+            'cons_acct_num': '0032', 'php':'LLL', 'port_type':'C',
+            'prev_acct_stat':'11', 'prev_cons_info_ind': '',
+            'prev_j1__cons_info_ind': '', 'prev_j2__cons_info_ind': '',
+            'prev_current_bal': 5
+        }]
+        self.assert_evaluator_correct(self.event, '9-3A Account Status current but PHP not current', expected)
+
+    def test_eval_9_3A_acct_stat_current_PHP_many_j1_segments(self):
+        # Create previous Account Activities data
+        self.create_test_9_3a_records(create_all_j1_segments=True)
+        # 1: HIT-multiple J1 Segments('','') only one record returned,
+        # 2: HIT, 3: NO-port_type='A', 4: NO-prev_cons_info_ind='A',
+        # 5: HIT-multiple J1 Segments prev_j1__cons_info_ind=['M',''],
+        # 6: NO-prev_current_bal=0, 7: NO-prev_acct_stat=10, 8: NO-php=O
+
+        # Create additional J1 segments for previous record
+        addl_j1_data = {
+            'account_activity':(32,36),
+            'cons_info_ind':('','')
+        }
+        self.prev_j1.append(self.create_bulk_JSegments('j1', addl_j1_data, 2))
+
+        # Create the segment data
+        expected = [{
+            'id': 42, 'activity_date': date(2019, 12, 31),
+            'cons_acct_num': '0032', 'php':'LLL', 'port_type':'C',
+            'prev_acct_stat':'11', 'prev_cons_info_ind': '',
+            'prev_j1__cons_info_ind': '', 'prev_j2__cons_info_ind': '',
+            'prev_current_bal': 5
+        }, {
+            'id': 43, 'activity_date': date(2019, 12, 31),
+            'cons_acct_num': '0033', 'php':'LLL', 'port_type':'O',
+            'prev_acct_stat':'11', 'prev_cons_info_ind': '',
+            'prev_j1__cons_info_ind': '', 'prev_j2__cons_info_ind': '',
+            'prev_current_bal': 10
+        }, {
+            'id': 46, 'activity_date': date(2019, 12, 31),
+            'cons_acct_num': '0036', 'php':'LLL', 'port_type':'O',
+            'prev_acct_stat':'11', 'prev_cons_info_ind': '',
+            'prev_j1__cons_info_ind': '', 'prev_j2__cons_info_ind': '',
+            'prev_current_bal': 25
         }]
         self.assert_evaluator_correct(self.event, '9-3A Account Status current but PHP not current', expected)
