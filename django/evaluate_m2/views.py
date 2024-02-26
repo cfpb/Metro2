b8ea072f54df682d84dd9bddbc2b76e5e6f673ea
Thuy Lam <thuy.lam@cfpb.gov>
2024-02-26 17:46:30 -0500

    update APIs and response and error responses.

--- django/evaluate_m2/views.py
+++ django/evaluate_m2/views.py
@@ -1,7 +1,13 @@
 import csv
-from django.http import Http404, HttpResponse, JsonResponse
+import logging
 from datetime import date
+from django.http import HttpResponse, JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from evaluate_m2.exception_utils import get_eval_results_not_found_exception
 from evaluate_m2.models import EvaluatorMetadata, EvaluatorResultSummary
 from parse_m2.models import Metro2Event
 
@@ -24,13 +30,14 @@ def download_evaluator_metadata(request):
 
     return response
 
+@api_view()
 def download_evaluator_results_csv(request, event_id, evaluator_name):
+    logger = logging.getLogger('views.download_evaluator_results_csv')
     try:
         eval_result_summary = EvaluatorResultSummary.objects.get(
             event=Metro2Event.objects.get(id=event_id),
             evaluator=EvaluatorMetadata.objects.get(name=evaluator_name))
         filename = f"{eval_result_summary.event.name}_{evaluator_name}_{date.today()}.csv"
-
         response = HttpResponse(
             content_type="text/csv",
             headers={"Content-Disposition": f"attachment; filename={filename}"}
@@ -47,19 +54,19 @@ def download_evaluator_results_csv(request, event_id, evaluator_name):
                 header_created=True
             else:
                 writer.writerow(eval_result.create_csv_row_data())
-
         return response
-    except Metro2Event.DoesNotExist:
-        msg = f"Event ID: {event_id} does not exist."
-        raise Http404(msg)
-    except EvaluatorMetadata.DoesNotExist:
-        msg = f"Evaluator: {evaluator_name} does not exist."
-        raise Http404(msg)
-    except EvaluatorResultSummary.DoesNotExist:
-        msg = f"Evaluator result does not exist for event ID {event_id} or evaluator {evaluator_name}."
-        raise Http404(msg)
+    except (
+        Metro2Event.DoesNotExist,
+        EvaluatorMetadata.DoesNotExist,
+        EvaluatorResultSummary.DoesNotExist
+    ) as e:
+        error = get_eval_results_not_found_exception(str(e), event_id, evaluator_name, request.path)
+        logger.error(error['message'])
+        return Response(error, status=status.HTTP_404_NOT_FOUND)
 
+@api_view()
 def download_evaluator_results(request, event_id, evaluator_name):
+    logger = logging.getLogger('views.download_evaluator_results')
     try:
         eval_result_summary = EvaluatorResultSummary.objects.get(
             event=Metro2Event.objects.get(id=event_id),
@@ -69,14 +76,12 @@ def download_evaluator_results(request, event_id, evaluator_name):
         for eval_result in eval_result_summary.evaluatorresult_set.all():
             results.append(eval_result.field_values)
         data = {'hits': results}
-
         return JsonResponse(data)
-    except Metro2Event.DoesNotExist:
-        msg = f"Event ID: {event_id} does not exist."
-        raise Http404(msg)
-    except EvaluatorMetadata.DoesNotExist:
-        msg = f"Evaluator: {evaluator_name} does not exist."
-        raise Http404(msg)
-    except EvaluatorResultSummary.DoesNotExist:
-        msg = f"Evaluator result does not exist for event ID {event_id} or evaluator {evaluator_name}."
-        raise Http404(msg)
+    except (
+        Metro2Event.DoesNotExist,
+        EvaluatorMetadata.DoesNotExist,
+        EvaluatorResultSummary.DoesNotExist
+    ) as e:
+        error = get_eval_results_not_found_exception(str(e), event_id, evaluator_name, request.path)
+        logger.error(error['message'])
+        return Response(error, status=status.HTTP_404_NOT_FOUND)
