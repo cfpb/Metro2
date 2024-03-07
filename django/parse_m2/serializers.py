28b69ea451eaef7dd7ef527eca38d5ac822dec2f
Thuy Lam <thuy.lam@cfpb.gov>
2024-03-07 16:12:41 -0500

    refactor

--- django/parse_m2/serializers.py
+++ django/parse_m2/serializers.py
@@ -3,13 +3,12 @@ from rest_framework import serializers
 from .models import AccountActivity, AccountHolder
 
 
-class AccountHolderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AccountHolder
-        exclude = ['data_file','activity_date']
-
-
 class AccountActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountActivity
         exclude = ['account_holder','cons_acct_num']
+
+class AccountHolderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountHolder
+        exclude = ['data_file','activity_date']
