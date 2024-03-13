16c55245918ffe95cc46625dd872c270bb199d74
Joshua Allen <130792942+BrewCityBoy@users.noreply.github.com>
2024-03-13 13:09:05 -0400

    generic not found component

---
+++ front-end/src/components/NotFound.tsx
@@ -0,0 +1,27 @@
+import type { ReactElement } from 'react'
+import { Link } from '@tanstack/react-router'
+
+export default function NotFound():  ReactElement {
+    return (
+            <div className='block u-center'>
+                <h2 id="title" className='h1'>
+                    This page doesn't exist.
+                </h2>
+                <p>
+                    We can't find the page you're looking for.
+                    If you believe you are seeing this in error
+                    double check that the URL is correct.
+                    If you need additional help, please contact an
+                    administrator for help.
+                </p>
+                <div className="m-btn-group">
+                    <Link to='/' className='a-btn'>Back to Metro 2 homepage</Link>
+                    <Link to='mailto:joshua.allen@cfpb.gov' className='a-btn a-btn__link'>Contact an administrator
+                        <svg xmlns="http://www.w3.org/2000/svg" className="cf-icon-svg cf-icon-svg__email a-btn__link" viewBox="0 0 17 19">
+                            <path d="M16.417 6.823v7.809a.557.557 0 0 1-.556.555H1.139a.557.557 0 0 1-.556-.555V6.823a.557.557 0 0 1 .556-.555h14.722a.557.557 0 0 1 .556.555m-14.722.92v6.146l4.463-2.89zm12.223-.364H3.082l4.463 3.257a1.777 1.777 0 0 0 1.91 0zM3.45 14.076h10.096L9.864 11.69a2.926 2.926 0 0 1-2.728 0zm11.855-.184v-6.15L10.842 11z"></path>
+                        </svg>
+                    </Link>
+                </div>
+            </div>
+    )
+}
--- front-end/src/pages/Event/route.tsx
+++ front-end/src/pages/Event/route.tsx
@@ -1,16 +1,34 @@
 import type { ReactElement } from 'react';
-import { createRoute, Outlet } from '@tanstack/react-router'
+import { createRoute, Outlet, createFileRoute } from '@tanstack/react-router'
 import rootRoute from '../rootRoute'
 import EventPage from './EventPage'
 
 export const eventRoute = createRoute({
   path: 'events/$eventId',
   getParentRoute: () => rootRoute,
-  component: (): ReactElement => (<Outlet />)
+  component: (): ReactElement => (<Outlet />),
+
+  notFoundComponent: () => {
+    return <p>This setting page doesn't exist!</p>
+  },
 })
 
 export const eventIndexRoute = createRoute({
   path: '/',
   getParentRoute: () => eventRoute,
   component: EventPage
+})
+
+export const Route = createFileRoute('/events/$eventId')({
+  component: () => {
+    return (
+      <div>
+        <p>Settings page</p>
+        <Outlet />
+      </div>
+    )
+  },
+  notFoundComponent: () => {
+    return <p>This setting page doesn't exist!</p>
+  },
 })
