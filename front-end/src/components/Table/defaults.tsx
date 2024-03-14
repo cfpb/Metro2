80571bb74cba92dc07076ece2efe36911e26aa1f
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2024-03-14 07:47:06 -0700

    Add table componennt and display table with fixture data on Event page.

---
+++ front-end/src/components/Table/defaults.tsx
@@ -0,0 +1,36 @@
+import type { ValueFormatterParams } from 'ag-grid-community'
+
+const numberFormatter = new Intl.NumberFormat('en', {
+  notation: 'standard'
+})
+
+export function formatNumber(val: number | null | undefined): string {
+  return val == null ? '' : numberFormatter.format(val) 
+}
+
+export const ColumnTypes = {
+  wrappableText: { 
+    wrapText: true,
+    autoHeight: true,
+    cellStyle: { wordBreak: 'normal' }
+  },
+  formattedNumber: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    valueFormatter: ( {value}: ValueFormatterParams<any, number | null> ): string => formatNumber(value),
+    cellClass: 'ag-right-aligned-cell',
+    headerClass: 'ag-right-aligned-header'
+  }
+}
+
+export const columnDefaults = {
+  sortable: true,
+  wrapHeaderText: true,
+  autoHeaderHeight: true
+}
+
+export const gridOptionDefaults = {
+  columnTypes: ColumnTypes,
+  enableCellTextSelection: true,
+  ensureDomOrder: true,
+  skipHeaderOnAutoSize: true
+}
--- front-end/src/pages/Event/EventPage.tsx
+++ front-end/src/pages/Event/EventPage.tsx
@@ -1,9 +1,23 @@
 import type { ReactElement } from 'react'
 import LocatorBar from 'components/LocatorBar/LocatorBar'
+import Table from 'components/Table/Table'
+import eventData from '../../fixtures/event.json'
+import columnDefinitions from './columnDefinitions'
+import type { EvaluatorMetadata } from './Event'
 
 export default function EventPage(): ReactElement {
+	const rowData = eventData.evaluators as EvaluatorMetadata[]
+
 	return (
-    <LocatorBar heading='Test bank exam'
-							  subhead='Data from 1/1/21 - 1/1/22'/>
+		<>
+    	<LocatorBar heading={ eventData.name }
+							    subhead={ `Data from ${ eventData.start_date } - ${ eventData.end_date }` }/>
+			<div className='block'>
+				<Table rows={ rowData } 
+							 columnDefinitions={ columnDefinitions }
+							 height='full' 
+							 resizableColumns={ false }/>
+			</div>
+		</>
 	)
 }
