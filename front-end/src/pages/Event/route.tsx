3d2477c417a13fc2b96c214a2486e97ff3dbb73e
Joshua Allen <130792942+BrewCityBoy@users.noreply.github.com>
2024-03-14 17:57:20 -0400

    suggested changes

--- front-end/src/pages/Event/route.tsx
+++ front-end/src/pages/Event/route.tsx
@@ -1,14 +1,10 @@
-import type { ReactElement } from 'react';
-import { createRoute, Outlet } from '@tanstack/react-router'
+import { createRoute } from '@tanstack/react-router'
 import rootRoute from '../rootRoute'
 import EventPage from './EventPage'
 
 export const eventRoute = createRoute({
   path: 'events/$eventId',
   getParentRoute: () => rootRoute,
-  component: (): ReactElement => (<Outlet />),
-
-  notFoundComponent: () => <p>This setting page doesn&apos;t exist!</p>,
 })
 
 export const eventIndexRoute = createRoute({
