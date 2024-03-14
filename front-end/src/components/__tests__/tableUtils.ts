76c8f937695d59fd3acda700cb46592eac8900e6
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2024-03-14 10:29:08 -0700

    Fix typo

--- front-end/src/components/__tests__/tableUtils.ts
+++ front-end/src/components/__tests__/tableUtils.ts
@@ -2,8 +2,8 @@ import { formatNumber } from '../Table/tableUtils'
 
 describe('formatNumber', () => {
 	it('returns a formatted string when passed a number', () => {
-    const formatttedLongNumber = formatNumber(123456789)
-    expect(formatttedLongNumber).toEqual('123,456,789')
+    const formattedLongNumber = formatNumber(123456789)
+    expect(formattedLongNumber).toEqual('123,456,789')
 
     const formattedShortNumber = formatNumber(123)
     expect(formattedShortNumber).toEqual('123')
