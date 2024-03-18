e6041340455dc3a11283fd7bbd2311b7b039a901
Thuy Lam <thuy.lam@cfpb.gov>
2024-03-18 16:42:15 -0400

    add test_serializers

---
+++ django/evaluate_m2/tests/test_serializers.py
@@ -0,0 +1,96 @@
+from datetime import date
+from django.test import TestCase
+from rest_framework.renderers import JSONRenderer
+
+from evaluate_m2.models import EvaluatorMetadata, EvaluatorResult, EvaluatorResultSummary
+from evaluate_m2.serializers import EventsViewSerializer
+from parse_m2.models import AccountActivity, AccountHolder, M2DataFile, Metro2Event
+
+
+class EventsViewSerializerTestCase(TestCase):
+    def setUp(self) -> None:
+        # Create an EvaluatorMetadata record
+        self.eval1 = EvaluatorMetadata.create_from_dict({
+            'id': 'ADDL-DOFD-1',
+            'name': 'Additional evaluator for Date of First Delinquency',
+            'description': 'description of addl-dofd-1',
+            'long_description': '',
+            'fields_used': 'account status;date of first delinquency',
+            'fields_display': 'amount past due;compliance condition code;current balance;date closed;original charge-off amount;scheduled monthly payment amount;special comment code;terms frequency',
+            'ipl': '',
+            'crrg_topics': '',
+            'crrg_page': '400',
+            'pdf_page': '',
+            'use_notes': '',
+            'alternative_explanation': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+            'risk_level': 'High'
+        })
+        # Create the parent records for the AccountActivity data
+        event = Metro2Event(id=1, name='test_exam')
+        event.save()
+        file = M2DataFile(event=event, file_name='file.txt')
+        file.save()
+        acct_holder = AccountHolder(
+            data_file=file,activity_date=date(2023, 11, 30), cons_acct_num="98765")
+        acct_holder.save()
+
+        self.acct_activity = AccountActivity(
+            account_holder=acct_holder,
+            activity_date=date(2023,11,20),
+            cons_acct_num="98765",
+            port_type="port_type",
+            acct_type="acct_type",
+            date_open=date(2020,3,17),
+            credit_limit=9000,
+            hcola=90210,
+            terms_dur="terms_dur",
+            terms_freq="terms_freq",
+            smpa=5,
+            actual_pmt_amt=201,
+            acct_stat="acct_stat",
+            pmt_rating="pmt_rating",
+            php="php",
+            spc_com_cd="spc_com_cd",
+            compl_cond_cd="compl_cond_cd",
+            current_bal=12345,
+            amt_past_due=111,
+            orig_chg_off_amt=0,
+            doai=date(2023,11,3),
+            dofd=None,
+            date_closed=None,
+            dolp=date(2023,1,1),
+            int_type_ind="int_type_ind",
+        )
+        self.acct_activity.save()
+
+        eval_rs = EvaluatorResultSummary(
+            event=event, evaluator=self.eval1, hits=2)
+        eval_rs.save()
+        eval_r = EvaluatorResult(
+            result_summary=eval_rs, date=date(2021, 1, 1),
+            field_values={'record': 1, 'acct_type':'y'},
+            source_record=self.acct_activity, acct_num='0032')
+        eval_r.save()
+
+        self.json_representation = {
+                'hits': 2, 'id': 'ADDL-DOFD-1',
+                'name': 'Additional evaluator for Date of First Delinquency',
+                'description': 'description of addl-dofd-1', 'long_description': '',
+                'fields_used': ['account status', 'date of first delinquency'],
+                'fields_display': ['amount past due', 'compliance condition code',
+                    'current balance', 'date closed', 'original charge-off amount', 'scheduled monthly payment amount', 'special comment code',
+                    'terms frequency'],
+                'ipl': '', 'crrg_topics': '', 'crrg_page': '400', 'pdf_page': '',
+                'use_notes': '', 'alternative_explanation': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 'risk_level': 'High'
+            }
+
+    def test_evaluator_metadata_serializer(self):
+        serializer = EventsViewSerializer(self.eval1)
+        self.assertEqual(serializer.data, self.json_representation)
+
+    def test_group_serializer_many_true(self):
+        eval_metadata = [self.eval1]
+        serializer = EventsViewSerializer(eval_metadata, many=True)
+        json_output = JSONRenderer().render(serializer.data)
+        expected = JSONRenderer().render([self.json_representation])
+        self.assertEqual(json_output, expected)
