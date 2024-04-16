10ba6945d42e5577bfb888a7a3c19deece103ba5
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2024-04-16 14:12:55 -0400

    Management command now includes user in auth group

--- django/seed-data.sh
+++ django/seed-data.sh
@@ -5,13 +5,13 @@ echo "Setting up the seed data..."
 
 echo
 echo "Adding a superuser with username 'admin'."
-python ./manage.py generate_admin_user --username=admin --password=admin
+python ./manage.py generate_admin_user --username=admin --password=admin --group=seed-group
 
 echo
 echo "Add Evaluator MetaData to the database"
 python manage.py import_evaluator_metadata -f evaluate_m2/m2_evaluators/eval_metadata.csv
 
 echo
-echo "Parse the data file and running the evaluators"
+echo "Parse the data file and run the evaluators"
 python manage.py parse_evaluate_local -e Sample-Dataset-007
 echo
--- django/users/management/commands/generate_admin_user.py
+++ django/users/management/commands/generate_admin_user.py
@@ -1,23 +1,44 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    help = "Creates an admin user non-interactively if it doesn't exist"
+    help = "Creates an admin user non-interactively if it doesn't exist. " + \
+           "Adds the user to a permissions group given by the --group argument"
 
     def add_arguments(self, parser):
         parser.add_argument('--username', help="Admin's username")
         parser.add_argument('--password', help="Admin's password")
+        parser.add_argument('--group', help="Auth group to add the user to")
 
     def handle(self, *args, **options):
         User = get_user_model()
+        username = options['username']
+        group_name = options['group']
 
-        if not User.objects.filter(username=options['username']).exists():
-            self.stdout.write(f"An admin user account was not found for username: {options['username']}")
-            User.objects.create_superuser(username=options['username'],
-                                          email='',
-                                          password=options['password'])
-            self.stdout.write(
-                self.style.SUCCESS(f"Finished creating admin user with username: {options['username']}.")
-            )
-        else:
-            self.stdout.write(f"An existing admin user account was found for username: {options['username']}")
+        try:
+            usr = User.objects.get(username=username)
+            self.stdout.write(f"An existing user account was found for username `{username}`.")
+            self.stdout.write("Ensuring the user account has admin permissions... ", ending="")
+            usr.is_active = True
+            usr.is_staff = True
+            usr.is_superuser = True
+            usr.save()
+            self.stdout.write("Done.")
+        except User.DoesNotExist:
+            self.stdout.write(f"Creating admin user account for username `{username}`... ", ending="")
+            usr = User.objects.create_superuser(username=username,
+                                                email='', password=options['password'])
+            self.stdout.write("Done.")
+        self.stdout.write(f"Admin user ID: {usr.id}.")
+
+        try:
+            grp = Group.objects.get(name=group_name)
+            self.stdout.write(f"Auth group exists with name {group_name}.")
+        except Group.DoesNotExist:
+            grp = Group.objects.create(name=group_name)
+            self.stdout.write(f"Created auth group with name {group_name}.")
+
+        self.stdout.write(f"Adding user `{username}` to group `{group_name}`.")
+        usr.groups.add(grp)
+        self.stdout.write(self.style.SUCCESS(f"Success: Admin user `{username}` exists with correct group permissions."))
