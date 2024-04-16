f7709fd96ec093fda0c6a593eea4559b26d0d27b
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2024-04-16 14:58:45 -0400

    Add user group to seed event in seed data

--- django/seed-data.sh
+++ django/seed-data.sh
@@ -14,4 +14,7 @@ python manage.py import_evaluator_metadata -f evaluate_m2/m2_evaluators/eval_met
 echo
 echo "Parse the data file and run the evaluators"
 python manage.py parse_evaluate_local -e Sample-Dataset-007
+
 echo
+echo "Add the event to the auth group"
+python manage.py add_auth_group_to_event --event=Sample-Dataset-007 --group=seed-group
