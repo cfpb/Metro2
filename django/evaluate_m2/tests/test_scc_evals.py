8ccc901a8a6a7baf6bf65f0e8843a69981bdc3fa
Thuy Lam <thuy.lam@cfpb.gov>
2024-04-30 18:19:56 -0400

    scc-status-3 eval and test

--- django/evaluate_m2/tests/test_scc_evals.py
+++ django/evaluate_m2/tests/test_scc_evals.py
@@ -457,6 +457,43 @@ class SCCEvalsTestCase(TestCase, EvaluatorTestHelper):
         }]
         self.assert_evaluator_correct(self.event, 'SCC-Status-2', expected)
 
+    def test_eval_scc_status_3(self):
+        # hits when both conditions met:
+        # 1. spc_com_cd == 'CP'
+        # 2. acct_stat == '13', '61', '62', '63', '64', '65'
+        # Create the Account Activities data
+        acct_date=date(2019, 12, 31)
+        activities = [
+            {
+                'id': 32, 'activity_date': acct_date, 'cons_acct_num': '0032',
+                'acct_stat':'13', 'spc_com_cd': 'CP'
+            }, {
+                'id': 33, 'activity_date': acct_date, 'cons_acct_num': '0033',
+                'acct_stat':'61', 'spc_com_cd': 'CP'
+            }, {
+                'id': 34, 'activity_date': acct_date, 'cons_acct_num': '0034',
+                'acct_stat':'11', 'spc_com_cd': 'CP'
+            }, {
+                'id': 35, 'activity_date': acct_date, 'cons_acct_num': '0035',
+                'acct_stat':'62', 'spc_com_cd': 'CK',
+            }]
+        for item in activities:
+            acct_record(self.data_file, item)
+        # 32: HIT, 33: HIT, 34: NO-acct_stat=11, 35: NO-spc_com_cd=CK
+        self.create_bulk_k2()
+
+        # Create the segment data
+        expected = [{
+            'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032',
+            'acct_stat': '13', 'spc_com_cd': 'CP', 'current_bal': 0,
+            'date_closed': None, 'k2__purch_sold_ind': 'a'
+        }, {
+            'id': 33, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0033',
+            'acct_stat': '61', 'spc_com_cd': 'CP', 'current_bal': 0,
+            'date_closed': None, 'k2__purch_sold_ind': None
+        }]
+        self.assert_evaluator_correct(self.event, 'SCC-Status-3', expected)
+
     def test_eval_scc_status_6(self):
         # hits when both conditions met:
         # 1. spc_com_cd == 'BF'
