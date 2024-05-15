5be4ce57f7d5898826a05bddc8385c01e6f8eca9
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2024-05-15 12:55:50 -0700

    Remove redundant data processing on account view.

--- front-end/src/pages/Account/AccountPage.tsx
+++ front-end/src/pages/Account/AccountPage.tsx
@@ -2,9 +2,8 @@ import { getRouteApi } from '@tanstack/react-router'
 import LocatorBar from 'components/LocatorBar/LocatorBar'
 import Table from 'components/Table/Table'
 import type { ReactElement } from 'react'
-import type { AccountRecord } from 'utils/constants'
 import { M2_FIELDS } from 'utils/constants'
-import { annotateData, generateColumnDefinitions } from 'utils/utils'
+import { generateColumnDefinitions } from 'utils/utils'
 import type Account from './Account'
 import AccountDownloader from './AccountDownloader'
 import Summary from './AccountSummary'
@@ -13,17 +12,10 @@ const routeApi = getRouteApi('/events/$eventId/accounts/$accountId')
 
 const colDefs = generateColumnDefinitions(M2_FIELDS, ['activity_date'])
 
-export const prepAccountData = (rows: AccountRecord[]): AccountRecord[] => {
-  // Add a new php1 value to each row based on first character of php
-  for (const row of rows) row.php1 = row.php?.charAt(0)
-  // Add annotations where available to row values
-  return annotateData(rows)
-}
-
 export default function AccountPage(): ReactElement {
   const { eventId }: { eventId: string } = routeApi.useParams()
   const accountData: Account = routeApi.useLoaderData()
-  const rows = prepAccountData(accountData.account_activity)
+  const rows = accountData.account_activity
 
   return (
     <>
