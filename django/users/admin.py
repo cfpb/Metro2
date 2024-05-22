33632fa0cc532e9b5350ae733e0409906671a1ec
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2024-05-22 16:49:15 -0400

    Customize admin view for User model
    Remove fields that are not relevant for our app

--- django/users/admin.py
+++ django/users/admin.py
@@ -1,3 +1,18 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    print(BaseUserAdmin.fieldsets)
+    # 'fieldsets' list is based on BaseUserAdmin.fieldsets, but
+    # with irrelevant fields removed for simplicity
+    # Fields removed: password, first and last name, email, groups,
+    # and user permissions
+    fieldsets = (
+        (None, {'fields': ('username', )}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', )}),
+        ('Important dates', {'fields': ('last_login', 'date_joined', )}))
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
