38bef12ad4b5b239f09d07ec26583874440953c2
Thuy Lam <thuy.lam@cfpb.gov>
2024-05-28 13:13:15 -0400

    Type-CreditLimit-4 evaluator and test

--- django/evaluate_m2/tests/test_type_evals.py
+++ django/evaluate_m2/tests/test_type_evals.py
@@ -321,3 +321,54 @@ class TypeEvalsTestCase(TestCase, EvaluatorTestHelper):
             'hcola': 0, 'terms_dur': '00'
         }]
         self.assert_evaluator_correct(self.event, 'Type-CreditLimit-3', expected)
+
+    def test_eval_type_credit_limit_4(self):
+        # Hits when all conditions are met:
+        # 1. port_type == 'C'
+        # 2. acct_type == '7A', '9B', '15', '43', '47', '89'
+        # 3. terms_freq != 'D'
+        # 4. credit_limit == 0
+
+        # Create the Account Activities data
+        acct_date=date(2019, 12, 31)
+        activities = [
+            {
+                'id': 32, 'activity_date': acct_date, 'cons_acct_num': '0032',
+                'port_type':'C', 'acct_type':'7A', 'terms_freq':'M',
+                'credit_limit': 0
+            }, {
+                'id': 33, 'activity_date': acct_date, 'cons_acct_num': '0033',
+                'port_type':'C', 'acct_type':'9B', 'terms_freq':'P',
+                'credit_limit': 0
+            }, {
+                'id': 34, 'activity_date': acct_date, 'cons_acct_num': '0034',
+                'port_type':'O', 'acct_type':'15', 'terms_freq':'O',
+                'credit_limit': 0
+            }, {
+                'id': 35, 'activity_date': acct_date, 'cons_acct_num': '0035',
+                'port_type':'C', 'acct_type':'0A', 'terms_freq':'M',
+                'credit_limit': 0
+            }, {
+                'id': 36, 'activity_date': acct_date, 'cons_acct_num': '0036',
+                'port_type':'C', 'acct_type':'43', 'terms_freq':'D',
+                'credit_limit': 0
+            }, {
+                'id': 37, 'activity_date': acct_date, 'cons_acct_num': '0037',
+                'port_type':'C', 'acct_type':'47', 'terms_freq':'P',
+                'credit_limit': 10
+            }]
+        for item in activities:
+            acct_record(self.data_file, item)
+        # 32: HIT, 33: HIT, 34:NO-port_type=O, 35: NO-acct_type=0A,
+        # 36: NO-terms_freq=D, 37: NO-credit_limit=10
+
+        expected = [{
+            'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032',
+            'acct_type': '7A', 'credit_limit': 0, 'port_type': 'C', 'terms_freq': 'M',
+            'hcola': 0, 'terms_dur': '00'
+        }, {
+            'id': 33, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0033',
+            'acct_type': '9B', 'credit_limit': 0, 'port_type': 'C', 'terms_freq': 'P',
+            'hcola': 0, 'terms_dur': '00'
+        }]
+        self.assert_evaluator_correct(self.event, 'Type-CreditLimit-4', expected)
