23c7cc50cb4cbf338983d7e2239747dcd40132c1
Thuy Lam <thuy.lam@cfpb.gov>
2024-05-28 14:23:05 -0400

    Type-TermsDuration-2 evaluator and test

--- django/evaluate_m2/tests/test_type_evals.py
+++ django/evaluate_m2/tests/test_type_evals.py
@@ -205,16 +205,67 @@ class TypeEvalsTestCase(TestCase, EvaluatorTestHelper):
             }]
         for item in activities:
             acct_record(self.data_file, item)
-        # 32: HIT, 33: HIT, 34:NO-port_type=0, 35: NO-acct_type=0A,
+        # 32: HIT, 33: HIT, 34:NO-port_type=O, 35: NO-acct_type=0A,
         # 36: NO-terms_freq=D, 37: NO-terms_dur=LOC
 
         expected = [{
             'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032',
-            'acct_type': '7A', 'port_type': 'C', 'terms_freq': 'M', 'terms_dur': '001',
+            'acct_type': '7A', 'port_type': 'C', 'terms_dur': '001', 'terms_freq': 'M',
             'credit_limit': 0, 'hcola': 0
         }, {
             'id': 33, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0033',
-            'acct_type': '9B', 'port_type': 'C', 'terms_freq': 'P', 'terms_dur': 'REV',
+            'acct_type': '9B', 'port_type': 'C', 'terms_dur': 'REV', 'terms_freq': 'P',
             'credit_limit': 0, 'hcola': 0
         }]
         self.assert_evaluator_correct(self.event, 'Type-TermsDuration-1', expected)
+
+    def test_eval_type_terms_dur_2(self):
+        # Hits when all conditions are met:
+        # 1. port_type == 'O'
+        # 2. acct_type == '2A', '8A', '8B', '18', '37'
+        # 3. terms_freq != 'D'
+        # 4. terms_dur != '001'
+
+        # Create the Account Activities data
+        acct_date=date(2019, 12, 31)
+        activities = [
+            {
+                'id': 32, 'activity_date': acct_date, 'cons_acct_num': '0032',
+                'port_type':'O', 'acct_type':'2A', 'terms_freq':'M',
+                'terms_dur': 'LOC'
+            }, {
+                'id': 33, 'activity_date': acct_date, 'cons_acct_num': '0033',
+                'port_type':'O', 'acct_type':'8A', 'terms_freq':'P',
+                'terms_dur': 'REV'
+            }, {
+                'id': 34, 'activity_date': acct_date, 'cons_acct_num': '0034',
+                'port_type':'C', 'acct_type':'8B', 'terms_freq':'O',
+                'terms_dur': 'LOC'
+            }, {
+                'id': 35, 'activity_date': acct_date, 'cons_acct_num': '0035',
+                'port_type':'O', 'acct_type':'0A', 'terms_freq':'M',
+                'terms_dur': 'REV'
+            }, {
+                'id': 36, 'activity_date': acct_date, 'cons_acct_num': '0036',
+                'port_type':'C', 'acct_type':'18', 'terms_freq':'D',
+                'terms_dur': 'LOC'
+            }, {
+                'id': 37, 'activity_date': acct_date, 'cons_acct_num': '0037',
+                'port_type':'C', 'acct_type':'37', 'terms_freq':'P',
+                'terms_dur': '001'
+            }]
+        for item in activities:
+            acct_record(self.data_file, item)
+        # 32: HIT, 33: HIT, 34:NO-port_type=C, 35: NO-acct_type=0A,
+        # 36: NO-terms_freq=D, 37: NO-terms_dur=001
+
+        expected = [{
+            'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032',
+            'acct_type': '2A', 'port_type': 'O', 'terms_dur': 'LOC', 'terms_freq': 'M',
+            'credit_limit': 0, 'hcola': 0
+        }, {
+            'id': 33, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0033',
+            'acct_type': '8A', 'port_type': 'O', 'terms_dur': 'REV', 'terms_freq': 'P',
+            'credit_limit': 0, 'hcola': 0
+        }]
+        self.assert_evaluator_correct(self.event, 'Type-TermsDuration-2', expected)
