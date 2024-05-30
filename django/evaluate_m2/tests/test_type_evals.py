b3012a37856bdfb6a5f0fe33d39146322eab759c
Thuy Lam <thuy.lam@cfpb.gov>
2024-05-30 11:53:44 -0400

    Revert "rename and reorder tests, no logic change"
    This reverts commit 9f57eda540baa0923f37e95f560da91e29bc26bc.

--- django/evaluate_m2/tests/test_type_evals.py
+++ django/evaluate_m2/tests/test_type_evals.py
@@ -21,6 +21,96 @@ class TypeEvalsTestCase(TestCase, EvaluatorTestHelper):
 
     ############################
     # Tests for the category 12 evaluators
+    def test_eval_12_installment_no_HCOLA(self):
+        # Hits when all conditions are met:
+        # 1. port_type == 'I'
+        # 2. acct_type == '00', '3A', '10', '91', '6A', '7B', '05',
+        #                 '0A', '0F', '47', '9A', '17', '01', '02',
+        #                 '03', '11', '13', '04', '20', '6D', '06'
+        # 3. terms_freq != 'D'
+        # 4. hcola <= 0
+
+        # Create the Account Activities data
+        activities = {'id':(32,33,34,35,36,37),
+                      'account_holder':('Z','Y','X','W','V','U'),
+                      'acct_type':('00','3A','12','91','00','3A'),
+                      'cons_acct_num':('0032','0033','0034','0035','0036','0037'), 'credit_limit':(10,20,30,40,50,60),
+                      'hcola':(-1,-1,-5,-5,0,5),
+                      'port_type':('I','M','I','I','I','I'),
+                      'terms_dur':('5','10','15','20','25','30'),
+                      'terms_freq':('P','W','P','D','W','P')}
+        # 1: HIT, 2: NO-port_type=M, 3: NO-acct_type=12, 4: NO-terms_freq=D,
+        # 5: HIT, 6: NO-hcola=5
+        self.create_bulk_activities(self.data_file, activities, 6)
+
+        expected = [{
+            'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032', 'acct_type': '00', 'port_type': 'I', 'hcola': -1,
+            'terms_freq': 'P', 'credit_limit': 10, 'terms_dur': '5'
+        }, {
+            'id': 36, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0036', 'acct_type': '00', 'port_type': 'I', 'hcola': 0,
+            'terms_freq': 'W', 'credit_limit': 50, 'terms_dur': '25'
+        }]
+        self.assert_evaluator_correct(self.event, 'Type-HCOLA-1', expected)
+
+    def test_eval_12_mortgage_no_HCOLA(self):
+        # Hits when all conditions are met:
+        # 1. port_type == 'M'
+        # 2. acct_type == '08', '19', '25', '26', '2C', '5A', '5B', '6B'
+        # 3. terms_freq != 'D'
+        # 4. hcola <= 0
+
+        # Create the Account Activities data
+        activities = {'id':(32,33,34,35,36,37),
+                      'account_holder':('Z','Y','X','W','V','U'),
+                      'acct_type':('08','19','11','25','2C','26'),
+                      'cons_acct_num':('0032','0033','0034','0035','0036','0037'), 'credit_limit':(10,20,30,40,50,60),
+                      'hcola':(-1,-1,-5,-5,0,5),
+                      'port_type':('M','I','M','M','M','M'),
+                      'terms_dur':('5','10','15','20','25','30'),
+                      'terms_freq':('P','W','P','D','W','P')}
+        # 1: HIT, 2: NO-port_type=I, 3: NO-acct_type=11, 4: NO-terms_freq=D,
+        # 5: HIT, 6: NO-hcola=5
+        self.create_bulk_activities(self.data_file, activities, 6)
+
+        expected = [{
+            'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032', 'acct_type': '08', 'port_type': 'M', 'hcola': -1,
+            'terms_freq': 'P', 'credit_limit': 10, 'terms_dur': '5'
+        }, {
+            'id': 36, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0036', 'acct_type': '2C', 'port_type': 'M', 'hcola': 0,
+            'terms_freq': 'W', 'credit_limit': 50, 'terms_dur': '25'
+        }]
+        self.assert_evaluator_correct(self.event, 'Type-HCOLA-2', expected)
+
+    def test_eval_12_open_no_HCOLA(self):
+        # Hits when all conditions are met:
+        # 1. port_type == 'O'
+        # 2. acct_type == '0C', '48', '77'
+        # 3. terms_freq != 'D'
+        # 4. hcola <= 0
+        # hcola cannot be an empty string or NULL by constraints
+
+        # Create the Account Activities data
+        activities = {'id':(32,33,34,35,36,37),
+                      'account_holder':('Z','Y','X','W','V','U'),
+                      'acct_type':('0C','48','11','77','0C','48'),
+                      'cons_acct_num':('0032','0033','0034','0035','0036','0037'), 'credit_limit':(10,20,30,40,50,60),
+                      'hcola':(-1,-1,-5,-5,0,5),
+                      'port_type':('O','I','O','O','O','O'),
+                      'terms_dur':('5','10','15','20','25','30'),
+                      'terms_freq':('P','W','P','D','W','P')}
+        # 1: HIT, 2: NO-port_type=I, 3: NO-acct_type=11, 4: NO-terms_freq=D,
+        # 5: HIT, 6: NO-hcola=5
+        self.create_bulk_activities(self.data_file, activities, 6)
+
+        expected = [{
+            'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032', 'acct_type': '0C', 'port_type': 'O', 'hcola': -1,
+            'terms_freq': 'P', 'credit_limit': 10, 'terms_dur': '5'
+        }, {
+            'id': 36, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0036', 'acct_type': '0C', 'port_type': 'O', 'hcola': 0,
+            'terms_freq': 'W', 'credit_limit': 50, 'terms_dur': '25'
+        }]
+        self.assert_evaluator_correct(self.event, 'Type-HCOLA-3', expected)
+
     def test_type_balance_1(self):
         # Hits when all conditions are met:
         # 1. port_type == 'I'
@@ -580,96 +670,6 @@ class TypeEvalsTestCase(TestCase, EvaluatorTestHelper):
             'port_type': 'O', 'spc_com_cd': 'AT', 'acct_stat': '', 'amt_past_due': 0
         }]
         self.assert_evaluator_correct(self.event, "Type-DateClosed-3", expected)
-
-    def test_eval_type_hcola_1(self):
-        # Hits when all conditions are met:
-        # 1. port_type == 'I'
-        # 2. acct_type == '00', '3A', '10', '91', '6A', '7B', '05',
-        #                 '0A', '0F', '47', '9A', '17', '01', '02',
-        #                 '03', '11', '13', '04', '20', '6D', '06'
-        # 3. terms_freq != 'D'
-        # 4. hcola <= 0
-
-        # Create the Account Activities data
-        activities = {'id':(32,33,34,35,36,37),
-                      'account_holder':('Z','Y','X','W','V','U'),
-                      'acct_type':('00','3A','12','91','00','3A'),
-                      'cons_acct_num':('0032','0033','0034','0035','0036','0037'), 'credit_limit':(10,20,30,40,50,60),
-                      'hcola':(-1,-1,-5,-5,0,5),
-                      'port_type':('I','M','I','I','I','I'),
-                      'terms_dur':('5','10','15','20','25','30'),
-                      'terms_freq':('P','W','P','D','W','P')}
-        # 1: HIT, 2: NO-port_type=M, 3: NO-acct_type=12, 4: NO-terms_freq=D,
-        # 5: HIT, 6: NO-hcola=5
-        self.create_bulk_activities(self.data_file, activities, 6)
-
-        expected = [{
-            'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032', 'acct_type': '00', 'port_type': 'I', 'hcola': -1,
-            'terms_freq': 'P', 'credit_limit': 10, 'terms_dur': '5'
-        }, {
-            'id': 36, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0036', 'acct_type': '00', 'port_type': 'I', 'hcola': 0,
-            'terms_freq': 'W', 'credit_limit': 50, 'terms_dur': '25'
-        }]
-        self.assert_evaluator_correct(self.event, 'Type-HCOLA-1', expected)
-
-    def test_eval_type_hcola_2(self):
-        # Hits when all conditions are met:
-        # 1. port_type == 'M'
-        # 2. acct_type == '08', '19', '25', '26', '2C', '5A', '5B', '6B'
-        # 3. terms_freq != 'D'
-        # 4. hcola <= 0
-
-        # Create the Account Activities data
-        activities = {'id':(32,33,34,35,36,37),
-                      'account_holder':('Z','Y','X','W','V','U'),
-                      'acct_type':('08','19','11','25','2C','26'),
-                      'cons_acct_num':('0032','0033','0034','0035','0036','0037'), 'credit_limit':(10,20,30,40,50,60),
-                      'hcola':(-1,-1,-5,-5,0,5),
-                      'port_type':('M','I','M','M','M','M'),
-                      'terms_dur':('5','10','15','20','25','30'),
-                      'terms_freq':('P','W','P','D','W','P')}
-        # 1: HIT, 2: NO-port_type=I, 3: NO-acct_type=11, 4: NO-terms_freq=D,
-        # 5: HIT, 6: NO-hcola=5
-        self.create_bulk_activities(self.data_file, activities, 6)
-
-        expected = [{
-            'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032', 'acct_type': '08', 'port_type': 'M', 'hcola': -1,
-            'terms_freq': 'P', 'credit_limit': 10, 'terms_dur': '5'
-        }, {
-            'id': 36, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0036', 'acct_type': '2C', 'port_type': 'M', 'hcola': 0,
-            'terms_freq': 'W', 'credit_limit': 50, 'terms_dur': '25'
-        }]
-        self.assert_evaluator_correct(self.event, 'Type-HCOLA-2', expected)
-
-    def test_eval_type_hcola_3(self):
-        # Hits when all conditions are met:
-        # 1. port_type == 'O'
-        # 2. acct_type == '0C', '48', '77'
-        # 3. terms_freq != 'D'
-        # 4. hcola <= 0
-        # hcola cannot be an empty string or NULL by constraints
-
-        # Create the Account Activities data
-        activities = {'id':(32,33,34,35,36,37),
-                      'account_holder':('Z','Y','X','W','V','U'),
-                      'acct_type':('0C','48','11','77','0C','48'),
-                      'cons_acct_num':('0032','0033','0034','0035','0036','0037'), 'credit_limit':(10,20,30,40,50,60),
-                      'hcola':(-1,-1,-5,-5,0,5),
-                      'port_type':('O','I','O','O','O','O'),
-                      'terms_dur':('5','10','15','20','25','30'),
-                      'terms_freq':('P','W','P','D','W','P')}
-        # 1: HIT, 2: NO-port_type=I, 3: NO-acct_type=11, 4: NO-terms_freq=D,
-        # 5: HIT, 6: NO-hcola=5
-        self.create_bulk_activities(self.data_file, activities, 6)
-
-        expected = [{
-            'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032', 'acct_type': '0C', 'port_type': 'O', 'hcola': -1,
-            'terms_freq': 'P', 'credit_limit': 10, 'terms_dur': '5'
-        }, {
-            'id': 36, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0036', 'acct_type': '0C', 'port_type': 'O', 'hcola': 0,
-            'terms_freq': 'W', 'credit_limit': 50, 'terms_dur': '25'
-        }]
-        self.assert_evaluator_correct(self.event, 'Type-HCOLA-3', expected)
 
     def test_eval_type_terms_dur_1(self):
         # Hits when all conditions are met:
