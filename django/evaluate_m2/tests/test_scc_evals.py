6939d085bc7c581a8aaee71c1a3ac687ae17b4ab
Thuy Lam <thuy.lam@cfpb.gov>
2024-06-17 14:18:24 -0400

    added SCC-DateClosed-2 evaluator and test

--- django/evaluate_m2/tests/test_scc_evals.py
+++ django/evaluate_m2/tests/test_scc_evals.py
@@ -681,6 +681,41 @@ class SCCEvalsTestCase(TestCase, EvaluatorTestHelper):
         self.assert_evaluator_correct(
             self.event, 'SCC-Balance-8', expected)
 
+    def test_eval_scc_dateclosed_2(self):
+        # hits when both conditions met:
+        # 1. spc_com_cd == 'CP'
+        # 2. date_closed != None
+        # Create the Account Activities data
+        acct_date=date(2019, 12, 31)
+        activities = [
+            {
+                'id': 32, 'activity_date': acct_date, 'cons_acct_num': '0032',
+                'spc_com_cd': 'CP', 'date_closed': date(2019, 12, 31)
+            }, {
+                'id': 33, 'activity_date': acct_date, 'cons_acct_num': '0033',
+                'spc_com_cd': 'CP', 'date_closed': date(2020, 12, 31)
+            }, {
+                'id': 34, 'activity_date': acct_date, 'cons_acct_num': '0034',
+                'spc_com_cd': 'CP', 'date_closed': None
+            }, {
+                'id': 35, 'activity_date': acct_date, 'cons_acct_num': '0035',
+                'spc_com_cd': 'BC', 'date_closed': date(2019, 12, 31)
+            }]
+        self.create_data(activities)
+        # 32: HIT, 33: No-DateClosed= None, 34: NO-spc_com_cd=BC
+
+        # Create the segment data
+        expected = [{
+            'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032',
+            'date_closed': date(2019, 12, 31), 'spc_com_cd': 'CP', 'acct_stat': '',
+            'current_bal': 0, 'k2__purch_sold_ind': 'a'
+        }, {
+            'id': 33, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0033',
+            'date_closed': date(2020, 12, 31), 'spc_com_cd': 'CP', 'acct_stat': '',
+            'current_bal': 0, 'k2__purch_sold_ind': None
+        }]
+        self.assert_evaluator_correct(self.event, 'SCC-DateClosed-2', expected)
+
     def test_eval_scc_dateclosed_3(self):
         # hits when both conditions met:
         # 1. spc_com_cd == 'M'
