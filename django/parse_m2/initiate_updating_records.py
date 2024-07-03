71c754e0ce859e788b17c3a5b6276f87955ea240
Thuy Lam <thuy.lam@cfpb.gov>
2024-07-03 16:48:02 -0400

    add mgmt command to update event records with previous_values

---
+++ django/parse_m2/initiate_updating_records.py
@@ -0,0 +1,31 @@
+import logging
+
+from parse_m2.models import Metro2Event
+
+############################################
+# Method to update existing M2Event activity records
+def update_event_records(event: Metro2Event):
+    logger = logging.getLogger('parse_m2.update_event_records')
+    total_updated = 0
+    total_not_updated = 0
+    # Retrieve Metro2Event records
+    logger.info(f"Fetching all records for event: {event.name}...")
+    record_set = event.get_all_account_activity()
+
+    # Updating the records
+    logger.info(f"Beginning to update all records for event: {event.name}...")
+    for record in record_set:
+        records = record_set.filter(cons_acct_num=record.cons_acct_num).order_by("activity_date")
+        # Retrieve current record index in sorted list
+        record_index = list(records).index(record)
+        if record_index and record_index > 0:
+            record.previous_values = records[record_index-1]
+            record.save()
+            # count total records updated
+            total_updated += 1
+        else:
+            # count total records not updated
+            total_not_updated +=1
+
+    logger.info(f"Previous records found and {total_updated} record{'s were' if total_updated > 1 else ' was'} updated.")
+    logger.info(f"Previous records not found and {total_not_updated} record{'s were' if total_updated > 1 else ' was'} not updated.")
