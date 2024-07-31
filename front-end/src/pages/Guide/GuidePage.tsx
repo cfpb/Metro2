84126683dd9635c457b795c4e8cec8e47cc5b2ce
Joshua Allen <130792942+BrewCityBoy@users.noreply.github.com>
2024-07-31 12:46:08 -0400

    lint output

--- front-end/src/pages/Guide/GuidePage.tsx
+++ front-end/src/pages/Guide/GuidePage.tsx
@@ -6,40 +6,40 @@ export default function GuidePage(): ReactElement {
 
   return (
     <Layout.Main layout="1-3">
-      <Layout.Wrapper>
-      < Layout.Sidebar id='sidebar'>
-        <div>
-          <ul>
-            <li>
-              Instructions for Metro 2
-
+        <Layout.Wrapper>
+          < Layout.Sidebar id='sidebar'>
+            <div>
               <ul>
                 <li>
-                Item 1
-                </li>
-                <li>
-                  Item 2
-                </li>
-                <li>
-                  Item 3
+                  Instructions for Metro 2
+
+                  <ul>
+                    <li>
+                    Item 1
+                    </li>
+                    <li>
+                      Item 2
+                    </li>
+                    <li>
+                      Item 3
+                    </li>
+                  </ul>
                 </li>
               </ul>
-            </li>
-          </ul>
-        </div>
-      </Layout.Sidebar>
-      <Layout.Content>
-      <h3>
-        Content
-      </h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Cum corrupti tempora nam nihil qui mollitia consectetur
-        corporis nemo culpa dolorum! Laborum at eos deleniti
-        consequatur itaque officiis debitis quisquam! Provident!
-      </p>
-      </Layout.Content>      
-      </Layout.Wrapper>
+            </div>
+          </Layout.Sidebar>
+          <Layout.Content>
+            <h3>
+              Content
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Cum corrupti tempora nam nihil qui mollitia consectetur
+              corporis nemo culpa dolorum! Laborum at eos deleniti
+              consequatur itaque officiis debitis quisquam! Provident!
+            </p>
+          </Layout.Content>      
+        </Layout.Wrapper>
       </Layout.Main>
   )
 }
