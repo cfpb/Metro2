a99822f34f6e94816220a6397708a93f9927124d
Joshua Allen <130792942+BrewCityBoy@users.noreply.github.com>
2024-07-31 12:20:24 -0400

    initial page layout

--- front-end/src/pages/Guide/GuidePage.tsx
+++ front-end/src/pages/Guide/GuidePage.tsx
@@ -1,18 +1,48 @@
 import LocatorBar from 'components/LocatorBar/LocatorBar'
+import { Layout } from 'design-system-react'
 import type { ReactElement } from 'react'
+import './GuidePage.less'
 
 export default function GuidePage(): ReactElement {
 
   return (
     <>
-      <LocatorBar
-        eyebrow="Metro2 User Guide"
-        heading='Metro 2 Need to Know(s)'
-        icon='book'
-      />
-      <div className='content-row'>
-        <p>Silly Billy! Trix are for kids.</p>
-      </div>
+      <Layout.Main layout="1-3">
+      <Layout.Wrapper>
+      < Layout.Sidebar id='sidebar'>
+        <div>
+          <ul>
+            <li>
+              Instructions for Metro 2
+
+              <ul>
+                <li>
+                Item 1
+                </li>
+                <li>
+                  Item 2
+                </li>
+                <li>
+                  Item 3
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </Layout.Sidebar>
+      <Layout.Content>
+      <h3>
+        Content
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Cum corrupti tempora nam nihil qui mollitia consectetur
+        corporis nemo culpa dolorum! Laborum at eos deleniti
+        consequatur itaque officiis debitis quisquam! Provident!
+      </p>
+      </Layout.Content>      
+      </Layout.Wrapper>
+      </Layout.Main>
     </>
   )
 }
