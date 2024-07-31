2f39647dcfc9119ca3998ad730aa633968713b60
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2024-07-31 16:09:14 -0400

    Reduce redudant code in eval test for balloon-balance-1

--- django/evaluate_m2/tests/test_balloon_evals.py
+++ django/evaluate_m2/tests/test_balloon_evals.py
@@ -17,76 +17,25 @@ class BalloonEvalsTestCase(TestCase, EvaluatorTestHelper):
         self.data_file.save()
 
     def test_eval_account_change_number_4(self):
-    # Hits when all conditions are met:
-    # 1. current_bal < K4.balloon_pmt_amt
+        # Hits when all conditions are met:
+        # 1. current_bal < K4.balloon_pmt_amt
 
         # Create the Account Activities data
         acct_date=date(2019, 12, 31)
-        activities = [
-            {
-                'id': 32, 'activity_date': acct_date, 'cons_acct_num': '0032',
-                'current_bal':0
-            }, {
-                'id': 33, 'activity_date': acct_date, 'cons_acct_num': '0033',
-                'current_bal':10
-            }, {
-                'id': 34, 'activity_date': acct_date, 'cons_acct_num': '0034',
-                'current_bal':20
-            }]
-        for item in activities:
-            acct_record(self.data_file, item)
-
-        k4_activities = [
-            {'id': 32, 'balloon_pmt_amt': 5},
-            {'id': 33, 'balloon_pmt_amt': 5},
-            {'id': 34, 'balloon_pmt_amt': 5}
-        ]
-        for item in k4_activities:
-            k4_record(item)
-        # 32: HIT, 33: NO-k4__balloon_pmt_amt < current_bal
-        # 34: NO-k4__balloon_pmt_amt < current_bal
+        acct_record(self.data_file, {'id': 32, 'activity_date': acct_date,
+                                     'cons_acct_num': '0032', 'current_bal':0})
+        acct_record(self.data_file, {'id': 33, 'activity_date': acct_date,
+                                     'cons_acct_num': '0033', 'current_bal':10})
+        acct_record(self.data_file, {'id': 34, 'activity_date': acct_date,
+                                     'cons_acct_num': '0034', 'current_bal':20})
+        k4_record({'id': 32, 'balloon_pmt_amt': 5})
+        k4_record({'id': 34, 'balloon_pmt_amt': 5})
+        # 32: HIT, 33: NO-missing K4 segment, 34: NO-k4__balloon_pmt_amt < current_bal
 
         # Create the segment data
         expected = [{
             'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032'
         }]
 
-        self.assert_evaluator_correct(
-            self.event, 'Balloon-Balance-1', expected)
-
-    def test_eval_account_change_number_4_missing_k4(self):
-    # Hits when all conditions are met:
-    # 1. current_bal < K4.balloon_pmt_amt
-
-        # Create the Account Activities data
-        acct_date=date(2019, 12, 31)
-        activities = [
-            {
-                'id': 32, 'activity_date': acct_date, 'cons_acct_num': '0032',
-                'current_bal':0
-            }, {
-                'id': 33, 'activity_date': acct_date, 'cons_acct_num': '0033',
-                'current_bal':10
-            }, {
-                'id': 34, 'activity_date': acct_date, 'cons_acct_num': '0034',
-                'current_bal':20
-            }]
-        for item in activities:
-            acct_record(self.data_file, item)
-
-        k4_activities = [
-            {'id': 32, 'balloon_pmt_amt': 5},
-            {'id': 34, 'balloon_pmt_amt': 5},
-        ]
-        for item in k4_activities:
-            k4_record(item)
-        # 32: HIT, 33: NO-missing K4 segment,
-        # 34: NO-k4__balloon_pmt_amt < current_bal
-
-        # Create the segment data
-        expected = [{
-            'id': 32, 'activity_date': date(2019, 12, 31),
-            'cons_acct_num': '0032', 'current_bal':0, 'k4__balloon_pmt_amt': 5
-        }]
         self.assert_evaluator_correct(
             self.event, 'Balloon-Balance-1', expected)
