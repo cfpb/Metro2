fbda006d91341aac5bf81deb02430ba223fb8f35
Thuy Lam <thuy.lam@cfpb.gov>
2024-08-08 12:39:01 -0400

    PROG-Status-3 evalutor and test

--- django/evaluate_m2/tests/test_prog_evals.py
+++ django/evaluate_m2/tests/test_prog_evals.py
@@ -150,13 +150,13 @@ class ProgEvalsTestCase(TestCase, EvaluatorTestHelper):
         prev_activities = [
             {
                 'id': 32, 'activity_date': prev_acct_date, 'cons_acct_num': '0032',
-                'acct_stat':'89', 'dofd': None
+                'acct_stat':'89'
             }, {
                 'id': 33, 'activity_date': prev_acct_date, 'cons_acct_num': '0033',
-                'acct_stat':'71', 'dofd': date(2019, 11, 1)
+                'acct_stat':'71'
             }, {
                 'id': 34, 'activity_date': prev_acct_date, 'cons_acct_num': '0034',
-                'acct_stat':'97', 'dofd': None
+                'acct_stat':'97'
             }]
         for r in prev_activities:
             acct_record(self.prev_data_file, r)
@@ -166,13 +166,13 @@ class ProgEvalsTestCase(TestCase, EvaluatorTestHelper):
         activities = [
             {
                 'id': 42, 'activity_date': acct_date, 'cons_acct_num': '0032',
-                'acct_stat':'97', 'dofd': date(2019, 12, 1)
+                'acct_stat':'97'
             }, {
                 'id': 43, 'activity_date': acct_date, 'cons_acct_num': '0033',
-                'acct_stat':'97', 'dofd': date(2019, 12, 10)
+                'acct_stat':'97'
             }, {
                 'id': 44, 'activity_date': acct_date, 'cons_acct_num': '0034',
-                'acct_stat':'77', 'dofd': date(2019, 10, 31)
+                'acct_stat':'77'
             }]
         for i in range(0, len(activities)):
             acct_record(self.data_file, activities[i])
@@ -183,3 +183,54 @@ class ProgEvalsTestCase(TestCase, EvaluatorTestHelper):
         expected = [
             {'id': 42, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032'}]
         self.assert_evaluator_correct(self.event, 'PROG-Status-2', expected)
+
+    def test_eval_prog_status_3(self):
+    # Hits when both conditions met:
+    # 1. previous_values__acct_stat == '13', '62'
+    # 2. acct_stat != '13', '62'
+    # 3. port_type == 'I', 'M'
+
+        # Create previous Account Activities data
+        prev_acct_date=date(2019, 11, 30)
+        prev_activities = [
+            {
+                'id': 32, 'activity_date': prev_acct_date, 'cons_acct_num': '0032',
+                'acct_stat':'13', 'port_type': 'I'
+            }, {
+                'id': 33, 'activity_date': prev_acct_date, 'cons_acct_num': '0033',
+                'acct_stat':'71', 'port_type': 'M'
+            }, {
+                'id': 34, 'activity_date': prev_acct_date, 'cons_acct_num': '0034',
+                'acct_stat':'13', 'port_type': 'I'
+            }, {
+                'id': 35, 'activity_date': prev_acct_date, 'cons_acct_num': '0035',
+                'acct_stat':'62', 'port_type': 'M'
+            }]
+        for r in prev_activities:
+            acct_record(self.prev_data_file, r)
+
+        # Create the Account Activities data
+        acct_date=date(2019, 12, 31)
+        activities = [
+            {
+                'id': 42, 'activity_date': acct_date, 'cons_acct_num': '0032',
+                'acct_stat':'97', 'port_type': 'I'
+            }, {
+                'id': 43, 'activity_date': acct_date, 'cons_acct_num': '0033',
+                'acct_stat':'97', 'port_type': 'M'
+            }, {
+                'id': 44, 'activity_date': acct_date, 'cons_acct_num': '0034',
+                'acct_stat':'62', 'port_type': 'I'
+            }, {
+                'id': 45, 'activity_date': acct_date, 'cons_acct_num': '0035',
+                'acct_stat':'77', 'port_type': 'A'
+            }]
+        for i in range(0, len(activities)):
+            acct_record(self.data_file, activities[i])
+        associate_previous_records(self.event)
+        # 42: HIT, 43: NO-previous_values__acct_stat=71,
+        # 44: NO-acct_stat=62, 45: NO-port_type='A'
+
+        expected = [
+            {'id': 42, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032'}]
+        self.assert_evaluator_correct(self.event, 'PROG-Status-3', expected)
