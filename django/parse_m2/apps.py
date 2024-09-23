ff712e24aecb34342ddc98c8c70d48c252322079
Thuy Lam <thuy.lam@cfpb.gov>
2024-09-23 14:20:05 -0700

    code to run job daily to disable inactive users

--- django/parse_m2/apps.py
+++ django/parse_m2/apps.py
@@ -5,3 +5,8 @@ class ParseM2Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'parse_m2'
     verbose_name='Parse M2'
+
+    def ready(self):
+        from users import task
+        
+        task.start()
---
+++ django/users/task.py
@@ -0,0 +1,29 @@
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from django.contrib.auth.models import User
+from datetime import timedelta
+from django.utils import timezone
+
+def disable_non_priviledged_inactive_users():
+    logger = logging.getLogger('users.tasks.disable_non_priviledged_inactive_users')
+    inactive_users = User.objects \
+        .filter(last_login__lt=timezone.now() - timedelta(days=90)) \
+        .exclude(is_superuser=True).exclude(is_active=False)
+    logger.info(f'{inactive_users.count()} non-privileged users will be deactivated and written to the database.')
+    inactive_users.update(is_active=False)
+    
+def disable_priviledged_inactive_users():
+    logger = logging.getLogger('users.tasks.disable_priviledged_inactive_users')
+    inactive_users = User.objects \
+        .filter(last_login__lt=timezone.now() - timedelta(days=45)) \
+        .exclude(is_superuser=False).exclude(is_active=False)
+    logger.info(f'{inactive_users.count()} privileged users will be deactivated and written to the database.')
+    inactive_users.update(is_active=False)
+       
+def start():
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(disable_non_priviledged_inactive_users, 'interval', days=1)
+        scheduler.add_job(disable_priviledged_inactive_users, 'interval', days=1)
+        scheduler.start()
+    
