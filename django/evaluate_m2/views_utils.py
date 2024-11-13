64d3d8ad8e071785d6471454dff04be0d9749f16
Thuy Lam <thuy.lam@cfpb.gov>
2024-11-13 13:48:50 -0800

    remove unused methods from views_utils and added get_s3_bucket

--- django/evaluate_m2/views_utils.py
+++ django/evaluate_m2/views_utils.py
@@ -1,6 +1,7 @@
-
 from django.conf import settings
 from django.contrib.auth.models import User
+
+from django_application.s3_utils import s3_resource
 
 def has_permissions_for_request(request, event) -> bool:
     if settings.SSO_ENABLED:
@@ -9,19 +10,6 @@ def has_permissions_for_request(request, event) -> bool:
     else:
         return True
 
-def get_randomizer(result_total, total_per_page) -> int:
-    randomizer = 1
-    if result_total > total_per_page:
-        randomizer = result_total // total_per_page
-    return randomizer
-
-def random_sample_id_list(eval_result_summary, number_of_results):
-    randomizer = get_randomizer(eval_result_summary.hits, number_of_results)
-
-    final_index = randomizer * number_of_results
-
-    eval_result_sample = eval_result_summary.evaluatorresult_set \
-        .only('source_record_id') \
-        .order_by('id')[0:final_index:randomizer]
-
-    return [result.source_record_id for result in eval_result_sample]
+def get_s3_bucket():
+    bucket = s3_resource()
+    return bucket
