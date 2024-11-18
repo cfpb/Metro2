e42ce3857672d4e7c008b51d6fbd9925e54f4eba
Thuy Lam <thuy.lam@cfpb.gov>
2024-11-18 13:33:46 -0800

    remove extra linebreak

--- django/evaluate_m2/views_utils.py
+++ django/evaluate_m2/views_utils.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 
-
 def has_permissions_for_request(request, event) -> bool:
     if settings.SSO_ENABLED:
         user = User.objects.get(username=request.user.username)
