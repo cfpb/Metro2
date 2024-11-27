c2ba2d5f7772d0846ad82a971ca7f29764bf63a4
Virginia Czosek <virginia.czosek@cfpb.gov>
2024-11-26 20:21:31 -0800

    Update Balance-APD-1 and deprecate Type-APD-1

--- django/evaluate_m2/tests/test_balance_evals.py
+++ django/evaluate_m2/tests/test_balance_evals.py
@@ -15,58 +15,36 @@ class BalanceEvalsTestCase(TestCase, EvaluatorTestHelper):
     ############################
     # Tests for the balance evaluators
     def test_eval_balance_apd_1(self):
-        # hits when both conditions met:
-        # 1. port_type == 'I'
-        # 2. acct_type == '3A', '13'
-        # 3. current_bal == 0
-        # 4. l1__change_ind == None
-        # 5. amt_past_due > 0
+        # Hits when both conditions met:
+        # 1. current_bal = 0
+        # 2. amt_past_due > 0
 
         # Create the Account Activities data
         acct_date=date(2019, 12, 31)
         activities = [
             {
                 'id': 32, 'activity_date': acct_date, 'cons_acct_num': '0032',
-                'port_type': 'I', 'acct_type':'3A', 'current_bal': 0,
-                'amt_past_due': 1
+                'current_bal': 0, 'amt_past_due': 5
             }, {
                 'id': 33, 'activity_date': acct_date, 'cons_acct_num': '0033',
-                'port_type': 'I','acct_type':'13', 'current_bal': 0,
-                'amt_past_due': 5
+                'current_bal': 10, 'amt_past_due': 15
             }, {
                 'id': 34, 'activity_date': acct_date, 'cons_acct_num': '0034',
-                'port_type': 'O', 'acct_type':'3A', 'current_bal': 0,
-                'amt_past_due': 10
+                'current_bal': -10, 'amt_past_due': 0
             }, {
                 'id': 35, 'activity_date': acct_date, 'cons_acct_num': '0035',
-                'port_type': 'I', 'acct_type':'00', 'current_bal': 0,
-                'amt_past_due': 15
+                'current_bal': 0, 'amt_past_due': 0
             }, {
-                'id': 36, 'activity_date': acct_date, 'cons_acct_num': '0036',
-                'port_type': 'I', 'acct_type':'00', 'current_bal': 1,
-                'amt_past_due': 20
-            }, {
-                'id': 37, 'activity_date': acct_date, 'cons_acct_num': '0037',
-                'port_type': 'I', 'acct_type':'00', 'current_bal': 0,
-                'amt_past_due': 25
-            }, {
-                'id': 38, 'activity_date': acct_date, 'cons_acct_num': '0038',
-                'port_type': 'I', 'acct_type':'00', 'current_bal': 0,
-                'amt_past_due': 0
+                'id': 35, 'activity_date': acct_date, 'cons_acct_num': '0035',
+                'current_bal': 10, 'amt_past_due': 5
             }]
         for item in activities:
             acct_record(self.data_file, item)
-
-        l1_segment = {'id': 37, 'change_ind': '1'}
-        l1_record(l1_segment)
-        # 32: HIT, 33: HIT, 34: NO-port_type=O,
-        # 35: NO-L1.acct_type=00, 36: NO-current_bal=1,
-        # 37: NO-l1__change_ind=1, 38: NO-amt_past_due=0
+        # 32: HIT, 33: NO-current_bal > 0, 34: NO-current_bal < 0,
+        # 35: NO-amt_past_due == 0, 36: NO-current_bal > amt_past_due
 
         expected = [{
             'id': 32, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0032',
-        }, {
-            'id': 33, 'activity_date': date(2019, 12, 31), 'cons_acct_num': '0033',
         }]
         self.assert_evaluator_correct(self.event, 'Balance-APD-1', expected)
 
