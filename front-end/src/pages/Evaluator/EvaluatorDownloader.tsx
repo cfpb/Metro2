294d078425da81e6ea6470f225b503ad1809e0c0
Virginia Czosek <virginia.czosek@cfpb.gov>
2025-04-21 10:17:03 -0700

    Download functionality cleanup

--- front-end/src/pages/Evaluator/EvaluatorDownloader.tsx
+++ front-end/src/pages/Evaluator/EvaluatorDownloader.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import DownloadModal from 'components/Modals/DownloadModal'
-import { Button } from 'design-system-react'
+import { Button, RadioButton } from 'design-system-react'
 import type { EvaluatorHits } from 'models/EvaluatorHits'
 import { evaluatorHitsQueryOptions } from 'models/EvaluatorHits'
 import type Event from 'pages/Event/Event'
@@ -35,11 +35,16 @@ export default function EvaluatorDownloader({
 }: EvaluatorDownloadInterface): ReactElement {
   const [isOpen, setIsOpen] = useState(false)
 
+  /**
+   * In case the user has applied filters and there are more results than are
+   * currently shown in the table, we set up a request for *all* the results
+   * for the current search params -- but cap it at 1 million because that's the
+   * max number of rows Excel can display.
+   */
   const { data, refetch } = useQuery<EvaluatorHits, Error, EvaluatorHits, string[]>(
     evaluatorHitsQueryOptions(String(eventData.id), evaluatorId, {
       ...query,
-      download: 'all',
-      page_size: currentHits
+      page_size: currentHits > 1_000_000 ? 1_000_000 : currentHits
     })
   )
 
@@ -52,49 +57,37 @@ export default function EvaluatorDownloader({
   }
 
   const onDownload = async (): Promise<void> => {
-    if (view === 'all') {
-      if (isFiltered) {
-        if (currentHits === rows.length) {
-          // download what's in the table because that's all the filtered results
-          const csv = generateDownloadData<AccountRecord>(
-            fields,
-            rows,
-            M2_FIELD_NAMES
-          )
-          try {
-            await downloadData(csv, `${eventData.name}_${evaluatorId}_filtered`)
-            setIsOpen(false)
-          } catch {
-            // TODO determine if we need to handle errors
-            setIsOpen(false)
-          }
-        } else {
-          // there are more filtered results than what's in the table
-          // get and download them all
-          await refetch()
-          const csv = generateDownloadData<AccountRecord>(
-            fields,
-            data.hits,
-            M2_FIELD_NAMES
-          )
-          try {
-            await downloadData(csv, `${eventData.name}_${evaluatorId}_filtered`)
-            setIsOpen(false)
-          } catch {
-            // TODO determine if we need to handle errors
-            setIsOpen(false)
-          }
-        }
-      } else {
-        const url = `/api/events/${eventData.id}/evaluator/${evaluatorId}/csv/`
-        // await writeURLToFile('all_evaluator_results.csv', url)
-        downloadFileFromURL(url)
-        setIsOpen(false)
-      }
+    if (view === 'all' && !isFiltered) {
+      // If you're viewing all results and haven't applied filters,
+      // download the pre-generated full results file
+      const url = `/api/events/${eventData.id}/evaluator/${evaluatorId}/csv/`
+      downloadFileFromURL(url)
+      setIsOpen(false)
     } else {
-      const csv = generateDownloadData<AccountRecord>(fields, rows, M2_FIELD_NAMES)
+      const ext = view === 'all' ? 'filtered' : 'sample'
+      let csv
+      // If you've applied filters and aren't viewing all the filtered results,
+      // get the rest of the filtered results and prep them for download
+      if (view === 'all' && isFiltered && currentHits > rows.length) {
+        await refetch()
+        csv = generateDownloadData<AccountRecord>(
+          fields,
+          data?.hits ?? [],
+          M2_FIELD_NAMES
+        )
+      } else {
+        // For all other cases, the results to download are already
+        // displayed in the table, so prep that data for download
+        // TODO: if there are fewer than 20 results for this evaluator,
+        // downloading with results toggle set to 'sample' will give you a front-end
+        // generated file with annotations and downloading from the 'all'
+        // view will get you the pre-generated results file without annotations.
+        // Maybe there should only be a single all results view when there's no sample?
+        csv = generateDownloadData<AccountRecord>(fields, rows, M2_FIELD_NAMES)
+      }
+      // Try downloading the data
       try {
-        await downloadData(csv, `${eventData.name}_${evaluatorId}_sample`)
+        await downloadData(csv, `${eventData.name}_${evaluatorId}_${ext}`)
         setIsOpen(false)
       } catch {
         // TODO determine if we need to handle errors
@@ -103,45 +96,22 @@ export default function EvaluatorDownloader({
     }
   }
 
-  let message = ''
-  if (view === 'all') {
-    message = isFiltered
-      ? 'all results for this evalutor with the filters you have applied'
-      : 'all results for this evaluator'
-  } else {
-    message =
-      totalHits > 20
-        ? 'the representative sample of results for this evaluator'
-        : 'all results for this evaluator'
-  }
+  let resultsMessage = 'all results for this evaluator'
+  if (view === 'sample' && totalHits > 20)
+    resultsMessage = 'a representative sample of results for this evaluator'
+  if (view === 'all' && isFiltered)
+    resultsMessage = 'all results for the applied filters'
+
   const header = (
-    <p>
-      <b>Clicking the save button will download {message}</b>.
-    </p>
-    // <div className=' block block__sub'>{message}</div>
-    // <fieldset className='o-form_fieldset block block__sub'>
-    //   <legend className='h4'>Choose .csv options:</legend>
-    //   <RadioButton
-    //     id='sample'
-    //     name='evaluator-download'
-    //     label={`Create a .csv with only the ${rows.length} result${
-    //       rows.length > 1 ? 's' : ''
-    //     } in the table`}
-    //     labelClassName=''
-    //     labelInline
-    //     defaultChecked
-    //     isLarge
-    //   />
-    //   <RadioButton
-    //     id='all'
-    //     name='evaluator-download'
-    //     label="Create a .csv containing all of this evaluator's results"
-    //     labelClassName=''
-    //     labelInline
-    //     inputRef={allResults}
-    //     isLarge
-    //   />
-    // </fieldset>
+    <RadioButton
+      id='all'
+      name='evaluator-download'
+      label={`Download a csv containing ${resultsMessage}`}
+      labelClassName=''
+      labelInline
+      isLarge
+      checked
+    />
   )
 
   return (
