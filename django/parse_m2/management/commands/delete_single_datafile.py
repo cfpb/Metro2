0f6fc0ff5b101a14fad99d6813c423292abae02e
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2025-05-16 16:33:47 -0400

    Improve param checking and logging output

--- django/parse_m2/management/commands/delete_single_datafile.py
+++ django/parse_m2/management/commands/delete_single_datafile.py
@@ -34,6 +34,12 @@ class Command(BaseCommand):
         file_name = options["file_name"]
         file_id = options["file_id"]
 
+        # Check params for validity
+        if file_name and file_id:
+            raise CommandError("Either file name or file ID must be provided, not both. Exiting.")
+        if not file_name and not file_id:
+            raise CommandError("Either file name or file ID must be provided. Exiting.")
+
         # Fetch the Metro2Event
         try:
             event = Metro2Event.objects.get(id=event_id)
@@ -44,19 +50,19 @@ class Command(BaseCommand):
         # Fetch the M2DataFile record
         try:
             if file_id:
-                logger.info(f"Searching for file with file_id {file_id} for event {event.id}...")
                 datafile =  M2DataFile.objects.get(event=event, id=file_id)
             elif file_name:
-                logger.info(f"Searching for file with file_name {file_name} for event {event.id}...")
                 datafile =  M2DataFile.objects.get(event=event, file_name=file_name)
+        except M2DataFile.DoesNotExist as e :
+            if file_id:
+                msg = f"file ID `{file_id}`"
             else:
-                raise CommandError("Either file name or file ID must be provided. Exiting.")
-        except M2DataFile.DoesNotExist:
-            raise CommandError(f"No such file found. Exiting.")
+                msg = f"file name `{file_name}`"
+            raise CommandError(f"No file found with {msg}. Exiting.")
 
         # Delete the file
         datafile.delete()
 
         logger.info(
-            self.style.SUCCESS(f"Finished deleting `{file_name}` for event: {event.name}.")
+            self.style.SUCCESS(f"Successfully deleted file {datafile.file_name} for event: {event.name}.")
         )
