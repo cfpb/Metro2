d274e31c8a3286c9cf18227dcbc16dff42558dc6
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2026-03-19 17:06:17 -0400

    Add collection to initiate_parsing methods

--- django/parse_m2/tests/test_initiate_parsing.py
+++ django/parse_m2/tests/test_initiate_parsing.py
@@ -53,6 +53,26 @@ class InitiateLocalParsingTestCase(TestCase):
         # the file contained 1997 parseable records
         self.assertEqual(AccountActivity.objects.count(), 1997)
 
+    def test_prepend_collection_onto_account_num(self):
+        parse_files_from_local_filesystem(self.event, collection="HEALTH.")
+
+        # one M2DataFile object for each file
+        self.assertEqual(M2DataFile.objects.count(), 3)
+        # 3 records in the first file, 2 in the second
+        self.assertEqual(AccountActivity.objects.filter(cons_acct_num__startswith="HEALTH.").count(), 5)
+
+    def test_prepend_collection_on_zipfile(self):
+        test_zip_location = os.path.join(
+            'parse_m2', 'tests','sample_files', 'test_local_zipped')
+        zip_event = Metro2Event.objects.create(
+            name="zipped exam", directory=test_zip_location
+        )
+        parse_files_from_local_filesystem(zip_event, collection="ZIP_EVENT")
+        # the zip contained 1 file
+        self.assertEqual(M2DataFile.objects.count(), 1)
+        # the file contained 1997 parseable records
+        self.assertEqual(AccountActivity.objects.filter(cons_acct_num__startswith="ZIP_EVENT").count(), 1997)
+
 
 class InitiateS3ParsingTestCase(TestCase):
     # Test for parsing files from the S3 bucket. Only run when testing manually.
