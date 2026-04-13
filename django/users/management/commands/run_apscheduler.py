f679f72e3a18abea61ff756cedafd2e924606405
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2026-04-13 16:48:03 -0400

    Fix linting

--- django/users/management/commands/run_apscheduler.py
+++ django/users/management/commands/run_apscheduler.py
@@ -20,18 +20,19 @@ from users.task import (
 logger = logging.getLogger(__name__)
 
 
-# The `close_old_connections` decorator ensures that database connections, that have become
-# unusable or are obsolete, are closed before and after your job has run. You should use it
-# to wrap any jobs that you schedule that access the Django database in any way. 
+# The `close_old_connections` decorator ensures that database connections
+# that have become unusable or are obsolete are closed before and after
+# your job has run. You should use it to wrap any jobs that you schedule
+# that access the Django database in any way.
 @util.close_old_connections
 def delete_old_job_executions(max_age=1_209_600):
   """
-  This job deletes APScheduler job execution entries older than `max_age` from the database.
-  It helps to prevent the database from filling up with old historical records that are no
-  longer useful.
-  
-  :param max_age: The maximum length of time to retain historical job execution records.
-                  Defaults to 14 days.
+  This job deletes APScheduler job execution entries older than `max_age`
+  from the database. It helps to prevent the database from filling up with
+  old historical records that are no longer useful.
+
+  :param max_age: The maximum length of time to retain historical job execution
+                  records. Defaults to 14 days.
   """
   DjangoJobExecution.objects.delete_old_job_executions(max_age)
 
