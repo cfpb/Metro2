4d5da946a64d105c9163df5bb8b65ac0ce3f4558
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2026-04-13 16:43:10 -0400

    close old database connections
    This may (?) resolve the issue where idle database connections stick
    around in the db for a long time

--- django/users/task.py
+++ django/users/task.py
@@ -1,14 +1,13 @@
 import logging
 from datetime import timedelta
 
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.utils import timezone
+from django_apscheduler import util
 
-from apscheduler.schedulers.background import BackgroundScheduler
 
-
+@util.close_old_connections
 def disable_non_privileged_inactive_users():
     logger = logging.getLogger('users.tasks.disable_non_privileged_inactive_users')
     inactive_users = User.objects.filter(
@@ -17,6 +16,7 @@ def disable_non_privileged_inactive_users():
     logger.info(f'Deactivating {inactive_users.count()} non-privileged users.')
     inactive_users.update(is_active=False)
 
+@util.close_old_connections
 def disable_privileged_inactive_users():
     logger = logging.getLogger('users.tasks.disable_privileged_inactive_users')
     inactive_users = User.objects.filter(
@@ -25,6 +25,7 @@ def disable_privileged_inactive_users():
     logger.info(f'Deactivating {inactive_users.count()} privileged users.')
     inactive_users.update(is_active=False)
 
+@util.close_old_connections
 def clear_expired_sessions():
     logger = logging.getLogger('users.tasks.clear_expired_sessions')
     logger.info("Removing expired session records from the database.")
