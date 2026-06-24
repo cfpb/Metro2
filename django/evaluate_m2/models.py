8eb77e19c347f9325d11eb565a351236908416a5
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2026-06-24 15:50:38 -0400

    Fix linting

--- django/evaluate_m2/models.py
+++ django/evaluate_m2/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.db.models import JSONField
+
 from django_prose_editor.fields import ProseEditorField
 
 from parse_m2.models import AccountActivity, Metro2Event
